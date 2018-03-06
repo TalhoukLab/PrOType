1f319e6480ee028159faa0d48239aaa5971be873
Dustin21 <dustin.bwj@gmail.com>
2018-03-06 07:57:26 -0800

    predict nstring single model

--- nanostring_classifier/step4_PredictNanoString.R
+++ nanostring_classifier/step4_PredictNanoString.R
@@ -1,5 +1,5 @@
 ###############################################################
-################ STEP 2: Predict All Nanostring ###############
+################ STEP 4: Predict All Nanostring ###############
 ###############################################################
 
 suppressPackageStartupMessages({
@@ -13,25 +13,20 @@ set.seed(2017)
 
 
 
-# import nanostring
-train.dat <- prep_data("ov.afc1_xpn", dir = "data/")
-nstring.batches <- load_nanostring("data/", train.dat$npcp[,-1])
+# load vancouver model
+Van_mod <- readr::read_rds("./outputs/fits/ov.afc1_xpn_adaboost.rds")
+genes <- Van_mod$names
 
-# import cut 1 fits
-fit.c1 <- readr::read_rds("outputs/fits/all_fits.rds")
+# load nanostring data
+nsdat <- load_nanostring("./data/nanostring/",genes)
 
-# predict nanostring
-pred.nano <- purrr::map2(fit.c1, names(fit.c1), function(x, y) {
-  purrr::map2(x, names(x), function(z, k) {
-    # uncomment below to write to file
-    #bc <- stringr::str_sub(y, nchar(y) - 2, nchar(y))
-    #fname <- paste0("outputs/predictions/nstring_allbatches_", bc, "_", k, ".rds")
-    #preds <- predict_nstring(z, nstring.batches)
-    #readr::write_rds(preds, path = fname)
-    #return(preds)
-    predict_nstring(z, nstring.batches)
-  })
-})
+# predict nanostring data
+pred.nano <- predict(Van_mod,nsdat,type = "class") %>% 
+  data_frame(ottaID=rownames(nsdat),preds=.)
+
+# Prediction file circulated to check that prediction match
+#preds <- read.csv("/Users/atalhouk/Repositories/NanoString/HGSCS/data/intermediate/predictions.csv", stringsAsFactors = TRUE)
+#table(preds$Adaboost.xpn,pred.ns_Van$preds)
 
 # write to file
 readr::write_rds(pred.nano, "outputs/predictions/nstring_allbatches.rds")
