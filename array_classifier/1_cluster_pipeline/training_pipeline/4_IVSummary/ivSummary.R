89872bac1b94e8c74b87c03bf4b8ad03b7a2db5e
Derek Chiu <dchiu@bccrc.ca>
2018-04-24 14:23:32 -0700

    tidy up ivSummary.R

--- array_classifier/1_cluster_pipeline/training_pipeline/4_IVSummary/ivSummary.R
+++ array_classifier/1_cluster_pipeline/training_pipeline/4_IVSummary/ivSummary.R
@@ -1,41 +1,36 @@
-# import dependencies
-suppressPackageStartupMessages({
-  require(tidyverse)
-  require(data.table)
-})
-
 # create internal validation table
 df <- list.files(fdir, recursive = TRUE, pattern = "*_train_*") %>%
   grep("Model", ., value = TRUE) %>%
-  purrr::map(., ~readRDS(paste0(fdir, "/", .x))) %>%
-  purrr::set_names({
+  purrr::map(~ readRDS(paste0(fdir, "/", .))) %>%
+  purrr::set_names(
     list.files(fdir, recursive = TRUE, pattern = "*_train_*") %>%
       grep("Model", ., value = TRUE) %>%
-      dirname() %>% basename()
+      dirname() %>%
+      basename()
+  ) %>%
+  purrr::modify_depth(2, ~ {
+    data.frame(measure = rownames(t(.)), t(.)) %>%
+      purrr::set_names(c("measure", "percentile_50", "percentile_5", "percentile_95"))
   }) %>%
-  purrr::modify_depth(., 2, function(x) {
-    data.frame(measure = rownames(t(x)), t(x)) %>%
-      set_names(c("measure", "percentile_50", "percentile_5", "percentile_95"))
+  purrr::imap(~ {
+    purrr::modify_depth(.x, 1, ~ data.frame(normalization = .y, .x))
   }) %>%
-  purrr::map2(., names(.), function(x, y) {
-    purrr::modify_depth(x, 1, ~data.frame(normalization = y, .x))
-  }) %>%
-  purrr::modify_depth(., 1, function(x) {
-    purrr::map2(x, names(x), ~data.frame(mod = .y, .x))
-  }) %>%
-  purrr::map(., ~data.table::rbindlist(.x)) %>%
-  data.table::rbindlist() %>% as_tibble() %>%
-  mutate(
+  purrr::modify_depth(1, ~ purrr::imap(~ data.frame(mod = .y, .x))) %>%
+  purrr::map(~ data.table::rbindlist(.)) %>%
+  data.table::rbindlist() %>%
+  tibble::as_tibble() %>%
+  dplyr::mutate(
     batch_correction = purrr::map(
-      strsplit(as.character(normalization), split = "_"), ~.x[3]) %>%
-      unlist %>% as.factor
-  ) %>%
-  mutate(
+      strsplit(as.character(normalization), split = "_"), ~ .[3]) %>%
+      unlist() %>%
+      as.factor(),
     normalization = purrr::map(
-      strsplit(as.character(normalization), split = "_"), ~.x[1]) %>% 
-      substr(start = 7, stop=nchar(.)) %>% unlist %>% as.factor
+      strsplit(as.character(normalization), split = "_"), ~ .[1]) %>%
+      substr(start = 7, stop = nchar(.)) %>%
+      unlist() %>%
+      as.factor()
   ) %>%
-  mutate(
+  dplyr::mutate(
     normalization = ifelse(normalization == "", "None", as.character(normalization)) %>%
       as.factor()
   )
