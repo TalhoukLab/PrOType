fc71be60acf4782c7b863fb371d8902e0df98d8f
Derek Chiu <dchiu@bccrc.ca>
2018-04-24 13:08:38 -0700

    tidy up summarize_rds_dir.R

--- array_classifier/1_cluster_pipeline/training_pipeline/utils/summarize_rds_dir.R
+++ array_classifier/1_cluster_pipeline/training_pipeline/utils/summarize_rds_dir.R
@@ -1,23 +1,15 @@
-suppressPackageStartupMessages({
-  require(tidyverse)
-  require(stringr)
-})
+suppressPackageStartupMessages(library(tidyverse))
 
 # input path from command line
 args <- commandArgs(TRUE)
 
 # extract file run names
-files <- list.files(path=args[1]) %>%
+files <- list.files(path = args[1]) %>%
   strsplit("_", fixed = TRUE) %>%
-  lapply('[', 1) %>%
-  unlist() %>%
-  data.frame(algoRuns = .) %>%
-  mutate(runID = stringr::str_extract(algoRuns, "[0-9]+"),
-         algo = stringr::str_extract(algoRuns, "[aA-zZ]+"))
+  map_chr(1) %>%
+  tibble(algoRuns = .,
+         runID = str_extract(algoRuns, "[0-9]+"),
+         algo = str_extract(algoRuns, "[aA-zZ]+"))
 
 # summarise number of reps per algo
-files.summarise <- files %>%
-  group_by(algo) %>%
-  summarise(count = length(algo))
-
-print(files.summarise)
+(files.summarise <- count(files, algo))
