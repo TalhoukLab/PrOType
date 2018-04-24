ba37025bfb509be5307fc9e23d00421ae8fae244
Derek Chiu <dchiu@bccrc.ca>
2018-04-24 14:26:21 -0700

    tidy up train_eval.R

--- array_classifier/1_cluster_pipeline/training_pipeline/4_IVSummary/train_eval.R
+++ array_classifier/1_cluster_pipeline/training_pipeline/4_IVSummary/train_eval.R
@@ -1,31 +1,24 @@
-#Packages----
-library(tidyverse)
+# Inputs ------------------------------------------------------------------
 
-#Inputs-----
 fdir <- fdir
 ndat <- ndat
 mname <- mname
 
-
-for(i in seq_along(ndat))
-{
+for (i in seq_along(ndat)) {
   print(ndat[i])
-  f <- grep(paste0("_train_eval_", ndat[i], ".rds"), list.files(paste0(fdir, ndat[i], "/", mname, "_", ndat[i], "/")), value = TRUE)
+  f <- grep(paste0("_train_eval_", ndat[i], ".rds"),
+            list.files(paste0(fdir, ndat[i], "/", mname, "_", ndat[i], "/")),
+            value = TRUE)
   print(f)
-  algs <- data.frame(f=f) %>%
-	separate(f,c("algs"),sep="_", extra="drop")
-  algs$algs[algs$algs=="second"] <- "svm"
-  f_Rfe <- grep("Rfe",f,value = TRUE)
-  f_rest <- f[!f%in%f_Rfe]
+  algs <- data.frame(f = f) %>%
+    tidyr::separate(f, "algs", sep = "_", extra = "drop")
+  algs$algs[algs$algs == "second"] <- "svm"
+  f_Rfe <- grep("Rfe", f, value = TRUE)
+  f_rest <- f[!f %in% f_Rfe]
 
-  m <- purrr::map(paste0(fdir,ndat[i],"/", mname, "_", ndat[i],  "/",f_rest), readRDS) %>%
-	unlist(.,recursive=FALSE)
+  res <- purrr::map(paste0(fdir, ndat[i], "/", mname, "_", ndat[i], "/", f_rest),
+                  readRDS) %>%
+    unlist(recursive = FALSE)
 
-  #mRFE <- purrr::map(paste0(fdir,ndat[i],"/", mname, "_", ndat[i],  "/",f_Rfe), readRDS) %>%
-  #	unlist(.,recursive=FALSE) %>% set_names(paste0(names(.),"Rfe"))
-
-  #res <- c(m, mRFE)
-  res <- m
-
-  saveRDS(res,paste0(fdir, ndat[i], "/data_pr_", ndat[i], "/train_eval_", ndat[i],  ".rds"))
+  saveRDS(res, paste0(fdir, ndat[i], "/data_pr_", ndat[i], "/train_eval_", ndat[i], ".rds"))
 }
