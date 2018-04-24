37a43f516984951e35996713f5aa9a3561f75eb1
Derek Chiu <dchiu@bccrc.ca>
2018-04-24 16:46:57 -0700

    tidy up nanostring_classifier/utils/utils.R

--- nanostring_classifier/utils/utils.R
+++ nanostring_classifier/utils/utils.R
@@ -1,259 +1,262 @@
 # Dependencies ----
 
-
 # load dependencies
 suppressPackageStartupMessages({
-  require(tidyverse)
+  library(tidyverse)
 })
-
 
 # Functions ----
 
 # build mapping
-build_mapping <- function(train.set)
-{
+build_mapping <- function(train.set) {
   # label mapping
-  labs <- c(1, 2, 3, 4)
-  if(train.set == "ov.afc1_cbt")
-  {
-    map <- data.frame(labs, labels = c("C1-MES",	"C5-PRO",	"C4-DIF",	"C2-IMM"))
-  } else if(train.set == "ov.afc1_xpn")
-  {
-    map <- data.frame(labs, labels = c("C2-IMM",	"C4-DIF", "C5-PRO",	"C1-MES"))
+  labs <- seq_len(4)
+  if (train.set == "ov.afc1_cbt") {
+    data.frame(labs, labels = c("C1-MES",	"C5-PRO",	"C4-DIF",	"C2-IMM"))
+  } else if (train.set == "ov.afc1_xpn") {
+    data.frame(labs, labels = c("C2-IMM",	"C4-DIF", "C5-PRO",	"C1-MES"))
   } else {
-    print("No valid training set specified")
+    stop("No valid training set specified")
   }
-  
-  return(map)
 }
 
-get_mapping <- function(dir = "data/")
-  #********************************************************************
-  # Import overlapping samples from TCGA and GSE and combine. Table
-  # also includes published labels.
-  #********************************************************************
-{
+#********************************************************************
+# Import overlapping samples from TCGA and GSE and combine. Table
+# also includes published labels.
+#********************************************************************
+get_mapping <- function(dir = "data/") {
   # TCGA overlap
-  tcga.mapped <- readr::read_csv(paste0(dir, "TCGA_sampleIDs_OTTA-Mapped.csv")) %>%
-    select(c(sampleID = TCGA, ottaID = `OTTA-ID`, published = `MOL-SUBTYPE-NAME (published)`))
-  
+  tcga.mapped <- paste0(dir, "TCGA_sampleIDs_OTTA-Mapped.csv") %>%
+    readr::read_csv() %>%
+    dplyr::select(c(
+      sampleID = TCGA,
+      ottaID = `OTTA-ID`,
+      published = `MOL-SUBTYPE-NAME (published)`
+    ))
+
   # GSE overlap
-  gse.mapped <- readr::read_csv(paste0(dir, "GSE9891_sampleIDs_OTTA-Mapped.csv")) %>%
-    select(c(sampleID = GSE9891, ottaID = `OTTA ID`, published = `MOL-SUBTYPE-NAME (published)`))
-  
+  gse.mapped <- paste0(dir, "GSE9891_sampleIDs_OTTA-Mapped.csv") %>%
+    readr::read_csv() %>%
+    dplyr::select(c(
+      sampleID = GSE9891,
+      ottaID = `OTTA ID`,
+      published = `MOL-SUBTYPE-NAME (published)`
+    ))
+
   # combine & drop NAs
-  map <- bind_rows(tcga.mapped, gse.mapped) %>% 
-    filter(published != "n/a")
-  
-  return(map)
+  dplyr::bind_rows(tcga.mapped, gse.mapped) %>%
+    dplyr::filter(published != "n/a")
 }
 
-get_nstring_overlap <- function(dir = "data/", map)
-  #********************************************************************
-  # Import nanostring data of overlapped samples and select those that
-  # match the mapping table returned from get_mapping()
-  #********************************************************************
-{
-  nanostring <- readr::read_rds(paste0(dir, "nanostring_aocs_tcga.rds")) %>%
+#********************************************************************
+# Import nanostring data of overlapped samples and select those that
+# match the mapping table returned from get_mapping()
+#********************************************************************
+get_nstring_overlap <- function(dir = "data/", map) {
+  nanostring <- paste0(dir, "nanostring_aocs_tcga.rds") %>%
+    readr::read_rds() %>%
     tibble::rownames_to_column("ottaID") %>%
-    inner_join(map, ., by = "ottaID") %>%
-    select(-c(sampleID, published)) %>%
+    dplyr::inner_join(map, ., by = "ottaID") %>%
+    dplyr::select(-c(sampleID, published)) %>%
     tibble::column_to_rownames("ottaID")
-  return(nanostring)
+  nanostring
 }
 
-predict_overlap <- function(fit, new.data)
-  #********************************************************************
-  # Simple predict function to take it a fit and predict on new.data
-  #********************************************************************
-{
-  pred <- splendid::prediction(mod = fit, 
-                               data = new.data, 
-                               class = 1:nrow(new.data), 
-                               threshold = 0) %>%
+#********************************************************************
+# Simple predict function to take it a fit and predict on new.data
+#********************************************************************
+predict_overlap <- function(fit, new.data) {
+  splendid::prediction(
+    mod = fit,
+    data = new.data,
+    class = seq_len(nrow(new.data)),
+    threshold = 0
+  ) %>%
     data.table::setattr(., "sampleID", rownames(new.data))
-  return(pred)
 }
 
-combine <- function(mapped.dat, nstring.overlap, nstring.pred)
-  #********************************************************************
-  # Combine nanostring, its predictions and array, then join with 
-  # mapping table. Return table containing predicted labels on
-  # nanostring and array in addition to published labels.
-  #********************************************************************
-{
+#********************************************************************
+# Combine nanostring, its predictions and array, then join with
+# mapping table. Return table containing predicted labels on
+# nanostring and array in addition to published labels.
+#********************************************************************
+combine <- function(mapped.dat, nstring.overlap, nstring.pred) {
   overlap <- nstring.overlap %>%
     tibble::rownames_to_column("ottaID") %>%
-    data.frame(., nstring = nstring.pred) %>%
-    select(ottaID, nstring) %>%
-    inner_join(mapped.dat, ., by = "ottaID") %>%
-    select(sampleID, ottaID, published, array, nstring)
-  return(overlap)
+    data.frame(nstring = nstring.pred) %>%
+    dplyr::select(ottaID, nstring) %>%
+    dplyr::inner_join(mapped.dat, ., by = "ottaID") %>%
+    dplyr::select(sampleID, ottaID, published, array, nstring)
+  overlap
 }
 
-evaluate_all <- function(x)
-  #********************************************************************
-  # Return list of evaluation measures. Output is required for ploting.
-  #********************************************************************
-{
+#********************************************************************
+# Return list of evaluation measures. Output is required for ploting.
+#********************************************************************
+evaluate_all <- function(x) {
   published_vs_array <- list(
     splendid::evaluation(x$published, x$array),
     caret::confusionMatrix(x$published, x$array)
   )
-  
   published_vs_nstring <- list(
     splendid::evaluation(x$published, x$nstring),
     caret::confusionMatrix(x$published, x$nstring)
   )
-  
   array_vs_nstring <- list(
     splendid::evaluation(x$array, x$nstring),
     caret::confusionMatrix(x$array, x$nstring)
   )
-  
-  return(list(
-    published_vs_array = published_vs_array,
-    published_vs_nstring = published_vs_nstring,
-    array_vs_nstring = array_vs_nstring
-  ))
+  tibble::lst(published_vs_array, published_vs_nstring, array_vs_nstring)
 }
 
-prep_data <- function(dataSet, dir = "./")
-  #********************************************************************
-  # Prepare data for cross-platform to Nanostring
-  #********************************************************************
-{
+#********************************************************************
+# Prepare data for cross-platform to Nanostring
+#********************************************************************
+prep_data <- function(dataSet, dir = "./") {
   # build mapping table
   map <- build_mapping(dataSet)
-  
+
   # read npcp normalized by housekeeping genes
-  npcp.tmp <- readr::read_rds(paste0(dir, "data_pr_", dataSet, "/npcp-hcNorm_",dataSet,".rds"))
-  
+  npcp.tmp <- paste0(dir, "data_pr_", dataSet, "/npcp-hcNorm_", dataSet, ".rds") %>%
+    readr::read_rds()
+
   # process rownames
   # i.e. OV_TCGA_FEAST_TCGA_31_1946_01_CEL_gz => OV_TCGA_FEAST_TCGA_31_1946_01
   # if the above is not an issue, comment out these lines
   charDrop <- function(x) stringr::str_sub(x, 0, nchar(x) - 7)
-  npcp.tmp <- npcp.tmp %>% data.frame(sampleID = rownames(.), .) %>%
-    mutate(sampleID = charDrop(as.character(sampleID))) %>%
-    tibble::column_to_rownames(., "sampleID")
-  
+  npcp.tmp <- npcp.tmp %>%
+    data.frame(sampleID = rownames(.), .) %>%
+    dplyr::mutate(sampleID = charDrop(as.character(sampleID))) %>%
+    tibble::column_to_rownames("sampleID")
+
   # read in diceR cluster labels (hgsc subtypes)
-  train.class.tmp <- readr::read_rds(paste0(dir, "data_pr_", dataSet, "/all_clusts_",dataSet,".rds"))[,1] %>% 
+  train.class.tmp <- paste0(dir, "data_pr_", dataSet, "/all_clusts_", dataSet, ".rds") %>%
+    readr::read_rds() %>%
+    magrittr::extract(, 1) %>%
     data.frame(labs = .) %>%
-    dplyr::inner_join(map, by = "labs") %>% .$labels
-  
+    dplyr::inner_join(map, by = "labs") %>%
+    magrittr::use_series("labels")
+
   # read in required prep files
   point_to_underscore <- function(x) stringr::str_replace(as.character(x), pattern = "\\.", replacement = "_")
   plus_to_underscore <- function(x) stringr::str_replace(as.character(x), pattern = "\\+", replacement = "_")
-  inclusion <- readr::read_csv(paste0(dir, "inclusion.csv")) %>% 
-    mutate(Label = point_to_underscore(Label)) %>%
-    mutate(Label = plus_to_underscore(Label))
-  keep <- inclusion %>% filter(post==1) %>% select(Label) # This is both cut 1 and 2
-  overlap <- inclusion %>% filter(post==3) %>% select(Label) 
-  original <- read.csv(paste0(dir, "Subtype_Original.csv"), stringsAsFactors = TRUE) 
+  inclusion <- readr::read_csv(paste0(dir, "inclusion.csv")) %>%
+    dplyr::mutate(Label = point_to_underscore(Label),
+                  Label = plus_to_underscore(Label))
+  keep <- inclusion %>%
+    dplyr::filter(post == 1) %>%
+    dplyr::select(Label) # This is both cut 1 and 2
+  overlap <- inclusion %>%
+    dplyr::filter(post == 3) %>%
+    dplyr::select(Label)
+  original <- read.csv(paste0(dir, "Subtype_Original.csv"), stringsAsFactors = TRUE)
   labels <- read.csv(paste0(dir, "NS_final_labels.csv"), stringsAsFactors = TRUE)
-  npcp <- npcp.tmp[rownames(npcp.tmp) %in% keep$Label,]
+  npcp <- npcp.tmp[rownames(npcp.tmp) %in% keep$Label, ]
   train.class <- train.class.tmp[rownames(npcp.tmp) %in% keep$Label]
-  
+
   df <- data.frame(lab = train.class, npcp)
-  
-  return(list(
+  list(
     npcp = df,
     original = original,
     labels = labels
-  ))
+  )
 }
 
-load_nanostring <- function(dir, genes)
-  #********************************************************************
-  # Load Nanostring data - all batches, aocs & tcga
-  #********************************************************************
-{
+#********************************************************************
+# Load Nanostring data - all batches, aocs & tcga
+#********************************************************************
+load_nanostring <- function(dir, genes) {
   # format gene data for extraction from nstring
   npcp <- data.frame(t(genes))
   colnames(npcp) <- genes
-  
+
   # import batch 1 nanostring
-  b1 <- read.csv(paste0(dir,"nanostring_classifier_data_batch1_20170217_updated.csv")) %>%
-    data.table::setattr(., 'batch', "b1")
-  b1.hk <- b1 %>% 
-    magrittr::extract(colnames(npcp)) %>% 
-    data.frame(.)
-  data.table::setattr(b1.hk, 'ottaID', b1$OTTA.ID)
-  
+  b1 <- paste0(dir,"nanostring_classifier_data_batch1_20170217_updated.csv") %>%
+    read.csv() %>%
+    data.table::setattr("batch", "b1")
+  b1.hk <- b1 %>%
+    magrittr::extract(colnames(npcp)) %>%
+    data.frame() %>%
+    data.table::setattr("ottaID", b1$OTTA.ID)
+
   # import batch 2 nanostring
-  b2 <- read.csv(paste0(dir,"nanostring_classifier_data_batch2_20170221.csv")) %>%
-    data.table::setattr(., 'batch', "b2")
-  b2.hk <- b2 %>% 
-    magrittr::extract(colnames(npcp)) %>% 
-    data.frame(.)
-  data.table::setattr(b2.hk, 'ottaID', b2$OTTA.ID)
-  
+  b2 <- paste0(dir,"nanostring_classifier_data_batch2_20170221.csv") %>%
+    read.csv() %>%
+    data.table::setattr("batch", "b2")
+  b2.hk <- b2 %>%
+    magrittr::extract(colnames(npcp)) %>%
+    data.frame() %>%
+    data.table::setattr("ottaID", b2$OTTA.ID)
+
   # import batch 3 nanostring
-  b3 <- read.csv(paste0(dir,"nanostring_classifier_data_batch3_20170307_updated_NCO.csv")) %>%
-    data.table::setattr(., 'batch', "b3")
-  b3.hk <- b3 %>% 
-    magrittr::extract(colnames(npcp)) %>% 
-    data.frame(.)
-  data.table::setattr(b3.hk, 'ottaID', b3$OTTA.ID)
-  
+  b3 <- paste0(dir,"nanostring_classifier_data_batch3_20170307_updated_NCO.csv") %>%
+    read.csv() %>%
+    data.table::setattr("batch", "b3")
+  b3.hk <- b3 %>%
+    magrittr::extract(colnames(npcp)) %>%
+    data.frame() %>%
+    data.table::setattr("ottaID", b3$OTTA.ID)
+
   # import batch 4 nanostring
-  b4 <- read.csv(paste0(dir,"nanostring_classifier_data_batch4_20170512.csv"), header = TRUE, stringsAsFactors = FALSE) %>%
-    data.table::setattr(., 'batch', "b4") 
-  b4.hk <- b4 %>% 
-    magrittr::extract(colnames(npcp)) %>% 
-    data.frame(.)
-  data.table::setattr(b4.hk, 'ottaID', b4$OTTA.ID)
-  
+  b4 <- paste0(dir,"nanostring_classifier_data_batch4_20170512.csv") %>%
+    read.csv(header = TRUE, stringsAsFactors = FALSE) %>%
+    data.table::setattr("batch", "b4")
+  b4.hk <- b4 %>%
+    magrittr::extract(colnames(npcp)) %>%
+    data.frame() %>%
+    data.table::setattr("ottaID", b4$OTTA.ID)
+
   # combine into list
-  test.dat <- list(
-    b1 = b1,
-    b2 = b2,
-    b3 = b3,
-    b4 = b4
-  )
-  
+  test.dat <- tibble::lst(b1, b2, b3, b4)
+
   # find intersecting genes
   genes <- colnames(npcp)
-  
+
   # extract intersecting genes
-  matched <- purrr::map(test.dat, function(x) x %>% 
-                          select(c("OTTA.ID", genes)) %>%
-                          mutate(batch = as.factor(attr(.,"batch")))) %>%
-    bind_rows() %>%
+  matched <- test.dat %>%
+    purrr::map(function(x) x %>%
+                 dplyr::select(c("OTTA.ID", genes)) %>%
+                 dplyr::mutate(batch = as.factor(attr(., "batch")))) %>%
+    dplyr::bind_rows() %>%
     tibble::column_to_rownames("OTTA.ID") %>%
     na.omit() %>%
-    data.table::setattr(., "batch", .$batch) %>%
-    select(-batch)
-  
-  return(matched)
+    data.table::setattr("batch", .$batch) %>%
+    dplyr::select(-batch)
+  matched
 }
 
-train <- function(x.processed, alg)
-  #********************************************************************
-  # Train model on prepared data
-  #   x.process: data.frame with genes as columns and rownames as
-  #              sampleIDs
-  #   alg: algorithm to fit to x.processed. See splendid for eligible
-  #        models.
-  #********************************************************************
-{
-  x <- x.processed[,-1]
-  lab <- x.processed[,1]
-  fit <- splendid::classification(data = x, class = lab, algorithms = alg, standardize = FALSE)
-  return(fit)
+#********************************************************************
+# Train model on prepared data
+#   x.process: data.frame with genes as columns and rownames as
+#              sampleIDs
+#   alg: algorithm to fit to x.processed. See splendid for eligible
+#        models.
+#********************************************************************
+train <- function(x.processed, alg) {
+  x <- x.processed[, -1]
+  lab <- x.processed[, 1]
+  splendid::classification(
+    data = x,
+    class = lab,
+    algorithms = alg,
+    standardize = FALSE
+  )
 }
 
-predict_nstring <- function(fit, nstring)
-  #********************************************************************
-  # Predict fit on nanostring data
-  #   fit: model fit returned from train()
-  #   nstring: nanostring data on which to predict
-  #********************************************************************
-{
-  pred <- splendid::prediction(mod = fit, data = nstring, class = rep(NA, nrow(nstring)), threshold = 0) %>%
-    data.table::setattr(., name = "ottaID", value = rownames(nstring)) %>%
-    data.table::setattr(., "batch", attr(nstring, "batch"))
+#********************************************************************
+# Predict fit on nanostring data
+#   fit: model fit returned from train()
+#   nstring: nanostring data on which to predict
+#********************************************************************
+predict_nstring <- function(fit, nstring) {
+  pred <- splendid::prediction(
+    mod = fit,
+    data = nstring,
+    class = rep(NA, nrow(nstring)),
+    threshold = 0
+  ) %>%
+    data.table::setattr("ottaID", rownames(nstring)) %>%
+    data.table::setattr("batch", attr(nstring, "batch"))
   df <- data.frame(ottaID = rownames(nstring), pred = pred)
-  return(pred)
+  pred
 }
