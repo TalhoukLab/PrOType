150888f893a67258bfaf73804f9bdcab8a3d3eca
Derek Chiu <dchiu@bccrc.ca>
2018-04-24 14:11:56 -0700

    tidy up ivCombine.R

--- array_classifier/1_cluster_pipeline/training_pipeline/4_IVSummary/ivCombine.R
+++ array_classifier/1_cluster_pipeline/training_pipeline/4_IVSummary/ivCombine.R
@@ -1,9 +1,8 @@
 # command line arguments
-args = commandArgs(trailingOnly=TRUE)
+args <- commandArgs(trailingOnly = TRUE)
 
-require(tidyverse)
 list.files(args[1], recursive = TRUE, pattern = "iv_summary_ov*") %>%
   grep("iv_summary_ov.*", ., value = TRUE) %>%
-  purrr::map(., ~readRDS(paste0(args[1], "/", .x))) %>%
+  purrr::map(~ readRDS(paste0(args[1], "/", .))) %>%
   data.table::rbindlist() %>%
-  readr::write_rds(., paste0(args[1], "/iv_summary_COMBINED.rds"))
+  readr::write_rds(paste0(args[1], "/iv_summary_COMBINED.rds"))
