76844797fa218e109405563371c305fb40a88202
Derek Chiu <dchiu@bccrc.ca>
2018-04-26 12:39:50 -0700

    build_mapping is used by both classifier pipelines, place in assets

--- nanostring_classifier/utils/utils.R
+++ nanostring_classifier/utils/utils.R
@@ -3,22 +3,12 @@
 # load dependencies
 suppressPackageStartupMessages({
   library(tidyverse)
+  library(here)
 })
 
-# Functions ----
+source(here("assets/utils.R"))
 
-# build mapping
-build_mapping <- function(train.set) {
-  # label mapping
-  labs <- seq_len(4)
-  if (train.set == "ov.afc1_cbt") {
-    data.frame(labs, labels = c("C1-MES", "C5-PRO", "C4-DIF", "C2-IMM"))
-  } else if (train.set == "ov.afc1_xpn") {
-    data.frame(labs, labels = c("C2-IMM", "C4-DIF", "C5-PRO", "C1-MES"))
-  } else {
-    stop("No valid training set specified")
-  }
-}
+# Functions ----
 
 #********************************************************************
 # Import overlapping samples from TCGA and GSE and combine. Table
