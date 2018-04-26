a425ae3b730a26cbc7b71e5d6ee7c09d3693ce04
Derek Chiu <dchiu@bccrc.ca>
2018-04-26 12:09:13 -0700

    fix spacing

--- nanostring_classifier/utils/utils.R
+++ nanostring_classifier/utils/utils.R
@@ -12,9 +12,9 @@ build_mapping <- function(train.set) {
   # label mapping
   labs <- seq_len(4)
   if (train.set == "ov.afc1_cbt") {
-    data.frame(labs, labels = c("C1-MES",	"C5-PRO",	"C4-DIF",	"C2-IMM"))
+    data.frame(labs, labels = c("C1-MES", "C5-PRO", "C4-DIF", "C2-IMM"))
   } else if (train.set == "ov.afc1_xpn") {
-    data.frame(labs, labels = c("C2-IMM",	"C4-DIF", "C5-PRO",	"C1-MES"))
+    data.frame(labs, labels = c("C2-IMM", "C4-DIF", "C5-PRO", "C1-MES"))
   } else {
     stop("No valid training set specified")
   }
