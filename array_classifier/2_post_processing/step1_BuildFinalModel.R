e68e5aa2072742f0a22d4e7c106f51a225e05a26
Derek Chiu <dchiu@bccrc.ca>
2018-04-26 16:39:44 -0700

    allow train_final to save fit object

--- array_classifier/2_post_processing/step1_BuildFinalModel.R
+++ array_classifier/2_post_processing/step1_BuildFinalModel.R
@@ -6,67 +6,36 @@ library(here)
 source(here("array_classifier/2_post_processing/utils/utils.R"))
 
 # import training data
-xpn.dat <- import_study("data/", "ov.afc1_xpn")
-cbt.dat <- import_study("data/", "ov.afc1_cbt")
+xpn.dat <- import_study("data", "ov.afc1_xpn")
+cbt.dat <- import_study("data", "ov.afc1_cbt")
+save_dir <- "outputs/fits"
 
 # fit algo
 seed <- readRDS("data/seed.rds")
 
 .Random.seed <- seed
-fit.xpn.adaboost <- train_final(xpn.dat, alg = "adaboost")
-readr::write_rds(
-  fit.xpn.adaboost,
-  paste0("outputs/fits/ov.afc1_xpn_adaboost.rds")
-)
+fit.xpn.adaboost <- train_final(xpn.dat, alg = "adaboost", file.name = file.path(save_dir, "ov.afc1_xpn_adaboost.rds"))
 
 .Random.seed <- seed
-fit.xpn.rf <- train_final(xpn.dat, alg = "rf")
-readr::write_rds(
-  fit.xpn.rf,
-  paste0("outputs/fits/ov.afc1_xpn_rf.rds")
-)
+fit.xpn.rf <- train_final(xpn.dat, alg = "rf", file.name = file.path(save_dir, "ov.afc1_xpn_rf.rds"))
 
 .Random.seed <- seed
-fit.xpn.mlr_ridge <- train_final(xpn.dat, alg = "mlr_ridge")
-readr::write_rds(
-  fit.xpn.mlr_ridge,
-  paste0("outputs/fits/ov.afc1_xpn_mlr_ridge.rds")
-)
+fit.xpn.mlr_ridge <- train_final(xpn.dat, alg = "mlr_ridge", file.name = file.path(save_dir, "ov.afc1_xpn_mlr_ridge.rds"))
 
 .Random.seed <- seed
-fit.xpn.mlr_lasso <- train_final(xpn.dat, alg = "mlr_lasso")
-readr::write_rds(
-  fit.xpn.mlr_lasso,
-  paste0("outputs/fits/ov.afc1_xpn_mlr_lasso.rds")
-)
+fit.xpn.mlr_lasso <- train_final(xpn.dat, alg = "mlr_lasso", file.name = file.path(save_dir, "ov.afc1_xpn_mlr_lasso.rds"))
 
 .Random.seed <- seed
-fit.cbt.adaboost <- train_final(cbt.dat, alg = "adaboost")
-readr::write_rds(
-  fit.cbt.adaboost,
-  paste0("outputs/fits/ov.afc1_cbt_adaboost.rds")
-)
+fit.cbt.adaboost <- train_final(cbt.dat, alg = "adaboost", file.name = file.path(save_dir, "ov.afc1_cbt_adaboost.rds"))
 
 .Random.seed <- seed
-fit.cbt.rf <- train_final(cbt.dat, alg = "rf")
-readr::write_rds(
-  fit.cbt.rf,
-  paste0("outputs/fits/ov.afc1_cbt_rf.rds")
-)
+fit.cbt.rf <- train_final(cbt.dat, alg = "rf", file.name = file.path(save_dir, "ov.afc1_cbt_rf.rds"))
 
 .Random.seed <- seed
-fit.cbt.mlr_ridge <- train_final(cbt.dat, alg = "mlr_ridge")
-readr::write_rds(
-  fit.cbt.mlr_ridge,
-  paste0("outputs/fits/ov.afc1_cbt_mlr_ridge.rds")
-)
+fit.cbt.mlr_ridge <- train_final(cbt.dat, alg = "mlr_ridge", file.name = file.path(save_dir, "ov.afc1_cbt_mlr_ridge.rds"))
 
 .Random.seed <- seed
-fit.cbt.mlr_lasso <- train_final(cbt.dat, alg = "mlr_lasso")
-readr::write_rds(
-  fit.cbt.mlr_lasso,
-  paste0("outputs/fits/ov.afc1_cbt_mlr_lasso.rds")
-)
+fit.cbt.mlr_lasso <- train_final(cbt.dat, alg = "mlr_lasso", file.name = file.path(save_dir, "ov.afc1_cbt_mlr_lasso.rds"))
 
 # build list of all fits
 be <- c("ov.afc1_xpn", "ov.afc1_cbt")
@@ -76,7 +45,7 @@ alg.list <- list()
 for (i in seq_along(be)) {
   for (j in seq_along(algs)) {
     fit.tmp <- readr::read_rds(
-      paste0("outputs/fits/", be[i], "_", algs[j], ".rds")
+      file.path(save_dir, paste0(be[i], "_", algs[j], ".rds"))
     )
     alg.list[[j]] <- fit.tmp
   }
@@ -84,4 +53,4 @@ for (i in seq_along(be)) {
   fit.c1[[i]] <- alg.list
 }
 names(fit.c1) <- be
-readr::write_rds(fit.c1, "outputs/fits/all_fits.rds")
+readr::write_rds(fit.c1, file.path(save_dir, "all_fits.rds"))
