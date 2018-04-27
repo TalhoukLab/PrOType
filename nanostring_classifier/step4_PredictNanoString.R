72ff1765159418fa0af413af023d87ec617ad500
Derek Chiu <dchiu@bccrc.ca>
2018-04-27 13:18:39 -0700

    fix paths

--- nanostring_classifier/step4_PredictNanoString.R
+++ nanostring_classifier/step4_PredictNanoString.R
@@ -8,11 +8,11 @@ source(here("nanostring_classifier/utils/utils.R"))
 set.seed(2017)
 
 # load vancouver model
-Van_mod <- readr::read_rds("./outputs/fits/ov.afc1_xpn_adaboost.rds")
+Van_mod <- readr::read_rds("outputs/fits/ov.afc1_xpn_adaboost.rds")
 genes <- Van_mod$names
 
 # load nanostring data
-nsdat <- load_nanostring("./data/",genes)
+nsdat <- load_nanostring("data", genes)
 
 # predict nanostring data
 pred.nano <- predict(Van_mod, nsdat, type = "class") %>%
