5ed3e250d631b08d6802897cd1548ebb6495dc60
Derek Chiu <dchiu@bccrc.ca>
2018-04-27 13:28:28 -0700

    vectorize import_study

--- array_classifier/2_post_processing/step1_BuildFinalModel.R
+++ array_classifier/2_post_processing/step1_BuildFinalModel.R
@@ -5,17 +5,16 @@
 library(here)
 source(here("array_classifier/2_post_processing/utils/utils.R"))
 
-# import training data
-xpn.dat <- import_study("data", "ov.afc1_xpn")
-cbt.dat <- import_study("data", "ov.afc1_cbt")
-save_dir <- "outputs/fits"
-
 # batch effects and algorithms
 be <- purrr::set_names(c("ov.afc1_xpn", "ov.afc1_cbt"))
 algs <- purrr::set_names(c("adaboost", "rf", "mlr_ridge", "mlr_lasso"))
 
+# import training data
+all.dat <- purrr::map(be, import_study, dir = "data")
+save_dir <- "outputs/fits"
+
 # fit algo
-purrr::walk2(list(xpn.dat, cbt.dat), be, ~ {
+purrr::walk2(all.dat, be, ~ {
   purrr::walk(algs, function(a) {
     train_final(.x, a, file.path(save_dir, paste0(.y, "_", a, ".rds")))
   })
