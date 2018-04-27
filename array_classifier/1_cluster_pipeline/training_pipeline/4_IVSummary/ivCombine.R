060324dd2b9d8bb0dfdd9e5115d7eac99ca5bd02
Derek Chiu <dchiu@bccrc.ca>
2018-04-27 15:43:59 -0700

    file.path faster than paste0

--- array_classifier/1_cluster_pipeline/training_pipeline/4_IVSummary/ivCombine.R
+++ array_classifier/1_cluster_pipeline/training_pipeline/4_IVSummary/ivCombine.R
@@ -3,6 +3,6 @@ args <- commandArgs(trailingOnly = TRUE)
 
 list.files(args[1], recursive = TRUE, pattern = "iv_summary_ov*") %>%
   grep("iv_summary_ov.*", ., value = TRUE) %>%
-  purrr::map(~ readRDS(paste0(args[1], "/", .))) %>%
+  purrr::map(~ readRDS(file.path(args[1], .))) %>%
   data.table::rbindlist() %>%
-  readr::write_rds(paste0(args[1], "/iv_summary_COMBINED.rds"))
+  readr::write_rds(file.path(args[1], "iv_summary_COMBINED.rds"))
