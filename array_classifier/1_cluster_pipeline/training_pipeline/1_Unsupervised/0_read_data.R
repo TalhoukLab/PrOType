e904c42d2d324d03c87804138f80648f5434b608
Derek Chiu <dchiu@bccrc.ca>
2018-05-01 11:44:14 -0700

    update inputs, outputs, simplify tdat, fix spacing

--- array_classifier/1_cluster_pipeline/training_pipeline/1_Unsupervised/0_read_data.R
+++ array_classifier/1_cluster_pipeline/training_pipeline/1_Unsupervised/0_read_data.R
@@ -1,24 +1,22 @@
 # This file is called in the beginning and not processed as part of the Q
-# input ndat and datadir pr
-# output a saved cdat
+# Inputs: dpath, ndat, pr, and datadir
+# Outputs: saved tdat, cdat
 
 library(diceR)
 library(tidyverse)
 
-
-# load data
+# Load data
 fname <- paste0(dpath, ndat, ".RData")
-tdat <- load(fname) %>% get(.) %>% t(.)
+tdat <- t(get(load(fname)))
 
-
-#Center and scale
-cdat <- switch (pr,
-	cs = prepare_data(tdat, scale = TRUE, type = "conventional"),
-	rs = prepare_data(tdat, scale = TRUE, type = "robust"),
-	ns = prepare_data(tdat, scale = FALSE)
+# Center and scale
+cdat <- switch(
+  pr,
+  cs = prepare_data(tdat, scale = TRUE, type = "conventional"),
+  rs = prepare_data(tdat, scale = TRUE, type = "robust"),
+  ns = prepare_data(tdat, scale = FALSE)
 )
 
-saveRDS(tdat,paste0(datadir,"/tdat_",ndat,".rds"))
-saveRDS(cdat,paste0(datadir,"/cdat_",ndat,".rds"))
-
-
+# Save as RDS objects
+saveRDS(tdat, paste0(datadir, "/tdat_", ndat, ".rds"))
+saveRDS(cdat, paste0(datadir, "/cdat_", ndat, ".rds"))
