e275166224bb700f6256a781002d7b1aa83ef290
mattpaletta <mattpaletta@gmail.com>
2018-05-08 10:10:27 -0700

    Made fixes

--- array_classifier/1_cluster_pipeline/training_pipeline/1_Unsupervised/2_impute_missing.R
+++ array_classifier/1_cluster_pipeline/training_pipeline/1_Unsupervised/2_impute_missing.R
@@ -2,5 +2,9 @@
 # Variable Inputs: algs, s
 
 #' Impute E to make E_knn and save to imputed directory
-E_knn <- apply(ssclust, 2:4, impute_knn, data = cdat, seed = 123)
-saveRDS(E_knn,paste0(sfdir,"/imputed_clust_",ndat,"/E_knn_",algs,s,"_",ndat,".rds"))
+pl_impute <- function(E, data, seed = 123, dir.name = ".") {
+	E_knn <- apply(E, 2:4, diceR::impute_knn, data = data, seed = seed)
+	saveRDS(E_knn,paste0(sfdir,"/imputed_clust_", ndat, "/E_knn_", algs, s, "_", ndat, ".rds"))
+}
+
+pl_impute(E=ssclust, data = cdat, dir.name = paste0(sfdir, "/imputed_clust_", ndat))
