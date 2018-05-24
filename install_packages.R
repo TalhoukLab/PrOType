7a8887b9a36e99db9f4eb07d97841cf6e1d0e64b
Matthew Paletta <mattpaletta@gmail.com>
2018-05-24 11:36:41 -0700

    Added install dplyr on older versions of R

--- install_packages.R
+++ install_packages.R
@@ -1,5 +1,13 @@
 pkg_list <- c("crayon", "here", "diceR") # however you want to get this
 
+compareVersion("3.3.1", "3.5.0")
+compareVersion("3.5.0", "3.5.0")
+compareVersion("3.5.0", "3.3.1")
+
+if (compareVersion("3.5.0", as.character(getRversion())) == 1) {
+  withCallingHandlers(install.packages("dplyr", repos="https://mirror.its.sfu.ca/mirror/CRAN/"), warning = function(w) stop(w))
+}
+
 withCallingHandlers(install.packages("crayon", repos="https://mirror.its.sfu.ca/mirror/CRAN/"), warning = function(w) stop(w))
 withCallingHandlers(install.packages("here", repos="https://mirror.its.sfu.ca/mirror/CRAN/"), warning = function(w) stop(w))
 withCallingHandlers(install.packages("diceR", repos="http://cran.stat.sfu.ca"), warning = function(w) stop(w))
