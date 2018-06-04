1a3b196c1083acaf8b424b430e0f8dd0c2d35193
Derek Chiu <dchiu@bccrc.ca>
2018-06-04 13:59:44 -0700

    update 1_Unsupervised Parameters.sh with cluster inputs

--- array_classifier/1_cluster_pipeline/training_pipeline/1_Unsupervised/Parameters.sh
+++ array_classifier/1_cluster_pipeline/training_pipeline/1_Unsupervised/Parameters.sh
@@ -6,13 +6,12 @@
 user="mpaletta"
 
 # data information
-dataSet="ov.afc1_cbt_sample"
+dataSet="ov.afc1_cbt"
 
 # Unsupervised parameters
-reps=5
+reps=1000
 k=4
-c=1 # use for determining splitting criterion (min 100 reps required)
-
+c=100 # use for determining splitting criterion (min 100 reps required)
 
 # directory inputs
 workDir="/PrOType/array_classifier/1_cluster_pipeline/training_pipeline/"
@@ -21,10 +20,7 @@ inputDir="/PrOType/array_classifier/1_cluster_pipeline/raw_data/raw_data/"
 logDir="/logs/"
 RPath="/usr/bin"
 
-
 # Developer params
 algs=(nmfbrunet nmflee distalgs rest)
-#algs=(distalgs rest)
-
 cons=(majority kmodes CSPA LCEcts LCEsrs LCEasrs)
 referenceClass="majority"
