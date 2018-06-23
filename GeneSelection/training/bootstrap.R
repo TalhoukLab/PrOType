4262a08518bce26d5a2adf873e4ab55e561ad0fa
Derek Chiu <dchiu@bccrc.ca>
2018-06-22 17:03:29 -0700

    add GeneSelection code

---
+++ GeneSelection/training/bootstrap.R
@@ -0,0 +1,14 @@
+runBootstrap <- function(output_dir, study, x, y, B,
+                         algs = c("lasso", "rf", "ada"),
+                         seed_boot = 2018, seed_alg = 2018) {
+  cli::cat_line("Running Bootstrap")
+  output_dir <- mkdir(file.path(output_dir, "output/training"))
+  args <- tibble::lst(output_dir, study, x, y, B, seed_boot, seed_alg)
+  purrr::walk(algs, ~ purrr::invoke(train_model, args, alg = .))
+}
+
+train_model <- function(output_dir, study, x, y, B, alg, seed_boot, seed_alg) {
+  cli::cat_line("Train ", alg,  " model and write output")
+  fit <- splendid::splendid_model(x, y, match_alg(alg), B, seed_boot, seed_alg)
+  readr::write_rds(fit, file.path(output_dir, paste0(study, "_fit_", alg, ".rds")))
+}
