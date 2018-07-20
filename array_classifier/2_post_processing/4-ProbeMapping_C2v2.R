a7e7ec5f3074ee73b8b0264dc119c55e9f3bf50f
Derek Chiu <dchiu@bccrc.ca>
2018-07-20 16:11:49 -0700

    refactor 4-ProbeMapping_C2v2

--- array_classifier/2_post_processing/4-ProbeMapping_C2v2.R
+++ array_classifier/2_post_processing/4-ProbeMapping_C2v2.R
@@ -1,75 +1,66 @@
-# Code to compute cluster centroids based on DiceR and map probes to genes
+# Code to compute cluster centroids based on diceR and map probes to genes
 # Note that if tidyverse is loaded code will not run
 
-# TODO:// Where do we get these libraries?
-library("hgug4112a.db")
-library("hgu133plus2.db")
-library(RColorBrewer)
+# devtools::install_url("https://bioconductor.org/packages/release/data/annotation/src/contrib/org.Hs.eg.db_3.6.0.tar.gz")
+# devtools::install_url("https://bioconductor.org/packages/release/data/annotation/src/contrib/hgug4112a.db_3.2.3.tar.gz")
+# devtools::install_url("https://bioconductor.org/packages/release/data/annotation/src/contrib/org.Hs.eg.db_3.6.0.tar.gz")
+library(hgug4112a.db)
+library(hgu133plus2.db)
 library(AnnotationDbi)
+library(RColorBrewer)
 library(magrittr)
 
-# Read in  ----
-# data
-# TODO:// Make this not a list.. (with a different name?)
-fdat <- c(trainSet)
-# TODO:// make this dynamic?
-ndat <- c("c2_xpn")
-dat_o <- purrr::map(fdat, ~ readRDS(file.path(outputDir, ., paste0("data_pr_", .), "cdat_", ., ".rds"))})
-        %>% magrittr::set_names(ndat)
+# Read in original data
+fdat <- testSet
+ndat <- gsub("ov.af", "", fdat)
+dat_o <- readRDS(file.path(outputDir, ., paste0("data_pr_", fdat), "cdat_", fdat, ".rds"))
 
-# obtain predictions
-clusts <- readRDS(file.path(outputDir, "predictions", paste0(ndat, ".rds")))
-
-dat <- purrr::map2(clusts, dat_o, function(x, y) {
-  C1 <- x
-  d <- data.frame(C1, y)
-  t <- y %>% t(.) %>% data.frame(.)
-  t2 <- data.frame(rownames(y), C1)
-  t$Probe <- rownames(t)
-  d2 <- d %>%
-    reshape2::melt(., id = "C1") %>%
-    dplyr::group_by(variable, C1) %>%
-    dplyr::summarise(median = median(value)) %>%
-    reshape2::dcast(., variable ~ C1, median)
-  return(list(full = t, centroids = d2, clusts = t2))
-}) %>% purrr::transpose(.)
+# Obtain predictions
+clusts <- readRDS(file.path(outputDir, "predictions", paste0(fdat, ".rds")))
+dat <- clusts %>%
+  purrr::map(~ {
+    d <- data.frame(C1 = ., dat_o)
+    clusts <- data.frame(Sample = rownames(dat_o), C1 = .)
+    full <- data.frame(t(y))
+    full$Probe <- rownames(full)
+    centroids <- d %>%
+      reshape2::melt(id = "C1") %>%
+      dplyr::group_by(variable, C1) %>%
+      dplyr::summarise(median = median(value)) %>%
+      reshape2::dcast(variable ~ C1, median, value.var = "median")
+    tibble::lst(full, centroids, clusts)
+  }) %>%
+  purrr::transpose()
 
 # Probe mapping
-centroid_dat <- purrr::map(dat$centroids, function(x) {
-  ID <- trimws(substring(as.character(x$variable), 2, 50))
-  AFX_lab <- AnnotationDbi::select(
-    hgu133plus2.db, ID,
-    c("SYMBOL")
-  ) %>%
-    dplyr::filter(!is.na(SYMBOL)) %>%
-    dplyr::filter(!duplicated(PROBEID))
-  x$Gene <- AFX_lab$SYMBOL[match(ID, AFX_lab$PROBEID)]
-  return(x)
+centroid_dat <- dat[["centroids"]] %>%
+  purrr::map(~ {
+    ID <- trimws(substring(as.character(.$variable), 2, 50))
+    AFX_lab <- AnnotationDbi::select(hgu133plus2.db, ID, "SYMBOL") %>%
+      dplyr::filter(!is.na(SYMBOL), !duplicated(PROBEID))
+    .$Gene <- AFX_lab$SYMBOL[match(ID, AFX_lab$PROBEID)]
+    .
+  })
+
+full_dat <- dat[["full"]] %>%
+  purrr::map(~ {
+    AFX_lab <- AnnotationDbi::select(hgu133plus2.db, .$Probe, "SYMBOL") %>%
+      dplyr::filter(!is.na(SYMBOL), !duplicated(PROBEID))
+    .$Gene <- AFX_lab$SYMBOL[match(.$Probe, AFX_lab$PROBEID)]
+    .
+  })
+
+# Write Full Data (preserve row names with write.csv)
+purrr::iwalk(full_dat, ~ {
+  write.csv(.x, file.path(outputDir, "evals", paste0("full_data_", .y, "_all.csv")))
 })
 
-
-full_dat <- purrr::map(dat$full, function(x) {
-  AFX_lab <- AnnotationDbi::select(
-    hgu133plus2.db, x$Probe,
-    c("SYMBOL")
-  ) %>%
-    dplyr::filter(!is.na(SYMBOL)) %>%
-    dplyr::filter(!duplicated(PROBEID))
-  x$Gene <- AFX_lab$SYMBOL[match(x$Probe, AFX_lab$PROBEID)]
-  return(x)
-})
-
-
-# Write Full Data
-purrr::map2(full_dat, names(full_dat), function(x, y) {
-  write.csv(x, file.path(outputDir, "evals", paste0("full_data/", y, "_all.csv")))
-})
-
-purrr::map2(dat$clusts, names(dat$clusts), function(x, y) {
-  write.csv(x, file.path(outputDir, "evals", paste0("full_data/", y, "-clusters.csv")))
+# Write Full Data clusters
+purrr::iwalk(dat$clusts, ~ {
+  readr::write_csv(.x, file.path(outputDir, "evals", paste0("full_data_", .y, "_clusters.csv")))
 })
 
 # Write Centroid Data
-purrr::map2(centroid_dat, names(centroid_dat), function(x, y) {
-  write.csv(x, file.path(outputDir, "evals", paste0("centroid_data_", y, ".csv")))
+purrr::iwalk(centroid_dat, ~ {
+  readr::write_csv(.x, file.path(outputDir, "evals", paste0("centroid_data_", .y, ".csv")))
 })
