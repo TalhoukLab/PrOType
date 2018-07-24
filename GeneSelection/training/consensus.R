399e7ddd42d7d913b9aabe97e5f791cc07eb10cf
Derek Chiu <dchiu@bccrc.ca>
2018-07-24 10:05:28 -0700

    use updated consensus script

--- GeneSelection/training/consensus.R
+++ GeneSelection/training/consensus.R
@@ -1,12 +1,5 @@
-compute_consensus <- function(preds_new, nsdat, batch = "b1") {
-  cli::cat_rule("Computing Consensus")
-  train <- define_batch(preds_new, nsdat, batch)
-  overlap <- define_overlap(preds_new, nsdat)
-  tibble::lst(train, overlap)
-}
-
-define_batch <- function(preds_new, nsdat, batch) {
-  cli::cat_line("Selecting batch ", batch, " data and labels")
+define_batch <- function(preds_new, nsdat, batch = "b1") {
+  cli::cat_line("Selecting batch ", batch, " data and labels where there is consensus")
   lab <- preds_new %>%
     dplyr::mutate(agree = ifelse(Adaboost.xpn == TCGA.Predicted.Subtype, 1, 0)) %>%
     dplyr::filter(is.na(published), agree == 1, Batch == batch) %>%
@@ -17,11 +10,11 @@ define_batch <- function(preds_new, nsdat, batch) {
 }
 
 define_overlap <- function(preds_new, nsdat) {
-  cli::cat_line("Selecting overlap data and labels")
+  cli::cat_line("Selecting overlap data and labels where there is consensus")
   lab <- preds_new %>%
     dplyr::mutate(agree = ifelse(Adaboost.xpn == TCGA.Predicted.Subtype, 1, 0)) %>%
     dplyr::filter(!is.na(published), agree == 1) %>%
-    dplyr::mutate_at(c("Adaboost.xpn", "TCGA.Predicted.Subtype","published"),
+    dplyr::mutate_at(c("Adaboost.xpn", "published"),
                      dplyr::funs(factor(gsub("-", "\\.", .))))
   dat <- filter_nsdat(nsdat, lab)
   check_data_order(lab, dat)
