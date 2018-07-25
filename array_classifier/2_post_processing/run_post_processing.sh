fdaedf79fa5e93011cdd3ab2090818cf76b90262
Derek Chiu <dchiu@bccrc.ca>
2018-07-25 16:57:51 -0700

    store post-processing directory as variable in bash

--- array_classifier/2_post_processing/run_post_processing.sh
+++ array_classifier/2_post_processing/run_post_processing.sh
@@ -23,24 +23,25 @@ echo 'testSet <- "'$testSet'"' >> $Rname
 echo 'datasets <- c("'$trainSet'", "'$trainSet2'")' >> $Rname
 echo 'outputDir <- "'$outputDir'"' >> $Rname
 echo 'dataDir <- "assets/data"' >> $Rname
+ppDir="array_classifier/2_post_processing/"
 
 echo 'cli::cat_line("Validating Results")' >> $Rname
-echo 'source("array_classifier/2_post_processing/0-validate_baseline_results.R")' >> $Rname
+echo 'source("'$ppDir'0-validate_baseline_results.R")' >> $Rname
 
-echo 'cli::cat_line("Starting step 1")' >> $Rname
-echo 'source("array_classifier/2_post_processing/1-evaluate_batch_effects.R")' >> $Rname
+echo 'cli::cat_line("Post-processing step 1")' >> $Rname
+echo 'source("'$ppDir'1-evaluate_batch_effects.R")' >> $Rname
 
-echo 'cli::cat_line("Starting step 2")' >> $Rname
-echo 'source("array_classifier/2_post_processing/2-internal_validity_plots.R")' >> $Rname
+echo 'cli::cat_line("Post-processing step 2")' >> $Rname
+echo 'source("'$ppDir'2-internal_validity_plots.R")' >> $Rname
 
-echo 'cli::cat_line("Starting step 3")' >> $Rname
-echo 'source("array_classifier/2_post_processing/3-predict_C2.R")' >> $Rname
+echo 'cli::cat_line("Post-processing step 3")' >> $Rname
+echo 'source("'$ppDir'3-predict_C2.R")' >> $Rname
 
-echo 'cli::cat_line("Starting step 4")' >> $Rname
-echo 'source("array_classifier/2_post_processing/4-probe_mapping_C2v2.R")' >> $Rname
+echo 'cli::cat_line("Post-processing step 4")' >> $Rname
+echo 'source("'$ppDir'4-probe_mapping_C2v2.R")' >> $Rname
 
-echo 'cli::cat_line("Starting step 5")' >> $Rname
-echo 'source("array_classifier/2_post_processing/5-mapping_signatures_C2.R")' >> $Rname
+echo 'cli::cat_line("Post-processing step 5")' >> $Rname
+echo 'source("'$ppDir'5-mapping_signatures_C2.R")' >> $Rname
 
 # echo 'for (dataset in unlist(strsplit("'"${dataSets[*]}"'", " "))) {' > $Rname
 #     echo 'cat("Starting Part 0\n")' >> $Rname
