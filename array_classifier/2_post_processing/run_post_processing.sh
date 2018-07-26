db075571d5ab0c1a0d52d0192c332e7e1079651e
Derek Chiu <dchiu@bccrc.ca>
2018-07-26 13:29:09 -0700

    move deprecated pp code

--- array_classifier/2_post_processing/run_post_processing.sh
+++ array_classifier/2_post_processing/run_post_processing.sh
@@ -42,18 +42,4 @@ echo 'source("'$ppDir'4-probe_mapping_C2v2.R")' >> $Rname
 echo 'cli::cat_line("Post-processing step 5")' >> $Rname
 echo 'source("'$ppDir'5-mapping_signatures_C2.R")' >> $Rname
 
-# echo 'for (dataset in unlist(strsplit("'"${dataSets[*]}"'", " "))) {' > $Rname
-#     echo 'cat("Starting Part 0\n")' >> $Rname
-#     echo 'cat(dataset, "\n")' >> $Rname
-#     echo 'output_dir <- "'$outputDir'/iv_summary"' >> $Rname
-#     echo 'source("array_classifier/2_post_processing/step0_InternalValidation.R")' >> $Rname
-# echo '}' >> $Rname
-# echo 'datasets <- unlist(strsplit("'"${dataSets[*]}"'", " "))' >> $Rname
-# echo 'algs <- purrr::set_names(unlist(strsplit("'"${classificationAlgs[*]}"'", " ")))' >> $Rname
-# echo 'output_dir <- "'$outputDir'"' >> $Rname
-# echo 'cat("Starting Part 1\n")' >> $Rname
-# echo 'source("array_classifier/2_post_processing/step1_BuildFinalModel.R")' >> $Rname
-# echo 'cat("Starting Part 2\n")' >> $Rname
-# echo 'source("array_classifier/2_post_processing/step2_ArrayValidation.R")' >> $Rname
-
 Rscript $Rname
