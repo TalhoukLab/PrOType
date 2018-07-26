a3221f485eaa358e79c5a5ab0338d16c193e90a7
Derek Chiu <dchiu@bccrc.ca>
2018-07-26 13:26:49 -0700

    move datasets to dev_params, update to cluster parameter values

--- Parameters.sh
+++ Parameters.sh
@@ -1,29 +1,26 @@
 #!/bin/bash
 
 # data information
-#dataSets=(ov.afc1_xpn ov.afc1_cbt ov.afc2_xpn ov.afc2_cbt)
-dataSets=(ov.afc1_cbt ov.afc1_xpn ov.afc2_xpn)
+trainSet="ov.afc1_xpn"
+trainSet2="ov.afc1_cbt"
+testSet="ov.afc2_xpn"
+
 shouldCompute=FALSE
 
 # Unsupervised parameters
-reps=5
+reps=1000
 k=4
-c=1 # use for determining splitting criterion (min 100 reps required)
+c=50 # use for determining splitting criterion (min 100 reps required)
 
 # Supervised parameters
-supervised_reps=10 # 5
+supervised_reps=500 # 5
 normalizeBy="None"
 threshold=0.0
 minVar=0
 normType="conventional"
 
 # IV Summary
-top=3
-
-# Post Processing
-trainSet="ov.afc1_xpn"
-trainSet2="ov.afc1_cbt"
-testSet="ov.afc2_xpn"
+top=5
 
 # directory inputs
 workDir="/PrOType/array_classifier/1_cluster_pipeline/"
