914cc9fe80eec8ee4a22a5f3aa80d4fb86f3cab7
Derek Chiu <dchiu@bccrc.ca>
2018-07-26 13:59:46 -0700

    fix algorithm seed explicitly

--- array_classifier/1_cluster_pipeline/3_Supervised/9_model_train.R
+++ array_classifier/1_cluster_pipeline/3_Supervised/9_model_train.R
@@ -67,7 +67,7 @@ train_supervised <- function(dataSet, algs, reps, inDir, outDir,
   cat("Running training algorithms:", algs, "-", reps, "\n")
   # train algorithms
   reps <- as.integer(reps)
-  sm_args <- list(data = data.train, class = make.names(class.train), n = 1, seed_boot = reps, threshold = threshold)
+  sm_args <- list(data = data.train, class = make.names(class.train), n = 1, seed_boot = reps, seed_alg = reps, threshold = threshold)
   sm <- switch(
     algs,
     first = purrr::invoke(splendid::splendid_model, sm_args,
