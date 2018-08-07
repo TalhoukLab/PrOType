c91b30a4af2c2c08b8ead2b91e733d761e0e14e1
Derek Chiu <dchiu@bccrc.ca>
2018-08-07 16:47:02 -0700

    add comments to final.R script

--- GeneSelection/scripts/final.R
+++ GeneSelection/scripts/final.R
@@ -1,3 +1,6 @@
+#' Prepare NanoString samples for prediction: make the column names
+#' syntactically valid, coerce to data frame, move OTTA id to row names, and
+#' keep only numeric columns (expression data)
 prepare_samples <- function(data) {
   data %>%
     `colnames<-`(make.names(colnames(.))) %>%
@@ -6,6 +9,8 @@ prepare_samples <- function(data) {
     dplyr::select_if(is.numeric)
 }
 
+#' Predict NanoString samples from final model. Combine the predicted
+#' assignments with predicted probability matrix.
 predict_samples <- function(mod, data) {
   predict(mod, data) %>%
     tibble::enframe(name = "ottaID", value = "predicted") %>%
