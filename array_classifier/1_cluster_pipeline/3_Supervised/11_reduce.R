d5a78ef2970eeebe6cc24f3c114e6f6502d50f38
Derek Chiu <dchiu@bccrc.ca>
2018-08-08 15:09:02 -0700

    attach magrittr in bash

--- array_classifier/1_cluster_pipeline/3_Supervised/11_reduce.R
+++ array_classifier/1_cluster_pipeline/3_Supervised/11_reduce.R
@@ -10,8 +10,6 @@
 #' @param outDir directory specifying the output location
 #' @return a data frame containing median and 95% confidence interval
 #' @author Last updated on 30/10/2017 by Dustin Johnson. Edited by Derek Chiu.
-
-library(magrittr)
 reduce_supervised <- function(dataSet, alg, outDir, fname = "Model", threshold = 0.0) {
   # store data directory path
   dirpath <- paste0(outDir, dataSet, "/", fname, "_", dataSet)
