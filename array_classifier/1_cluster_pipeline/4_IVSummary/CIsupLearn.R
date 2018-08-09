cf283947024ccd7f102746451ffc8c44cc19e5ca
Derek Chiu <dchiu@bccrc.ca>
2018-08-09 16:50:35 -0700

    add alg param to create_ci, simplify sort_best

--- array_classifier/1_cluster_pipeline/4_IVSummary/CIsupLearn.R
+++ array_classifier/1_cluster_pipeline/4_IVSummary/CIsupLearn.R
@@ -14,48 +14,45 @@ matrix.sort <- function(matrix) {
 }
 
 # Create CIs
-create_ci <- function(df) {
-  cat("Creating CIs\n")
+create_ci <- function(df, alg) {
+  cli::cat_line("Creating CIs for ", alg)
   res <- paste(sprintf("%.2f", df[, "50%"]),
                paste0("(", sprintf("%.2f", df[, "5%"]),
                       "-", sprintf("%.2f", df[, "95%"]), ")")) %>%
     data.frame() %>%
+    magrittr::set_colnames(alg) %>%
     magrittr::set_rownames(rownames(df))
   res
 }
 
 # Sort best
 sort_best <- function(train_eval, top = 5) {
-  cat("Sorting best\n")
-  te <- purrr::map(train_eval, function(x) {
-    x %>%
-      as.data.frame() %>%
+  cli::cat_line("Combining median evaluation metrics")
+  te <- purrr::map(train_eval, ~ {
+    as.data.frame(.) %>%
       dplyr::mutate(logloss = -logloss) %>%
       t() %>%
-      magrittr::extract(, 1)}) %>%
-    data.frame(.)
+      magrittr::extract(, 1)
+  }) %>%
+    data.frame()
   stopifnot(rownames(te)[1] == "logloss")
 
-  cat("Before calculating best\n")
-  algsfull <- colnames(te)
-  df <- te %>%
-    apply(1, function(x) algsfull[order(rank(-x, ties.method = "random"))]) %>%
+  cli::cat_line("Calculating top ", top, " algorithms by rank aggregation")
+  top.list <- te %>%
+    apply(1, function(x) colnames(.)[order(-x, sample(length(x)))]) %>%
     t() %>%
     RankAggreg::RankAggreg(., ncol(.), method = "GA",
-                           verbose = FALSE, maxIter = 2000)
+                           verbose = FALSE, maxIter = 2000) %>%
+    purrr::pluck("top.list") %>%
+    magrittr::extract(seq_len(top))
 
-  cat("Calculating best\n")
-  best <- te[, df$top.list]
-  res <- best[c("auc", "accuracy", "f1.1", "f1.2", "f1.3", "f1.4"),
-              seq_len(top)] %>%
-    data.frame() %>%
-    magrittr::set_colnames(colnames(best)[seq_len(top)])
-  tibble::lst(best, res)
+  cli::cat_line("Extracting top AUC, accuracy, by-class F1")
+  te[c("auc", "accuracy", "f1.X1", "f1.X2", "f1.X3", "f1.X4"), top.list]
 }
 
 # Relabel classes
 relabel_classes <- function(df, FinalR_lab) {
-  cat("Relaballing classes\n")
+  cli::cat_line("Relabelling classes")
   a <- table(FinalR_lab[, 1], FinalR_lab[, "CL"])
   equi <- data.frame(class = rownames(matrix.sort(a)),
                      label = colnames(matrix.sort(a)))
@@ -83,14 +80,13 @@ final <- fdat %>%
     train_eval <- readRDS(paste0(inDir, ., "/data_pr_", ., "/train_eval_", ., ".rds"))
     bests <- sort_best(train_eval, top = top)
     ci <- train_eval %>%
-      purrr::map(~ {
-        data.frame(.) %>%
+      purrr::imap(~ {
+        data.frame(.x) %>%
           t() %>%
-          create_ci()
+          create_ci(.y)
       }) %>%
-      data.frame() %>%
-      magrittr::set_names(names(train_eval))
-    ci[, colnames(bests$res)] # CI across evaluation metrics
+      data.frame()
+    ci[, colnames(bests)]
   }) %>%
   magrittr::set_names(fdat)
 
