950f687a30f86002d3f92c706804272b43375f9c
Derek Chiu <dchiu@bccrc.ca>
2018-08-09 11:00:54 -0700

    invoke common cc_args for ssclust

--- array_classifier/1_cluster_pipeline/1_Unsupervised/1_clust_data.R
+++ array_classifier/1_cluster_pipeline/1_Unsupervised/1_clust_data.R
@@ -3,40 +3,30 @@
 
 outputFile <- paste0(sfdir, "/rds_out_", ndat, "/", algs, s, "_", ndat)
 
-cat("Checking previous Input\n")
+cli::cat_line("Checking previous input")
 if (file.exists(paste0(outputFile, ".rds")) && !shouldCompute) {
-      cat("File already exists, skipping.\n")
-      quit(status = 0)
+  cli::cat_line("File already exists, skipping")
+  quit(status = 0)
 }
 
-cat(outputFile)
-library(diceR)
-r<-1
+cc_args <- tibble::lst(
+  data = cdat,
+  nk = k,
+  reps = 1,
+  prep.data = "none",
+  seed.data = s,
+  file.name = outputFile
+)
 
-ssclust <- switch(algs,
-nmfbrunet = {
-        consensus_cluster(data = cdat, nk = k, reps = r,
-        algorithms = "nmf", nmf.method = "brunet",
-        prep.data = "none", seed.data = s,
-        file.name = outputFile)
-    },
-    nmflee = {
-        consensus_cluster(data = cdat, nk = k, reps = r,
-        algorithms = "nmf", nmf.method = "lee",
-        prep.data = "none", seed.data = s,
-        file.name = outputFile)
-    },
-    distalgs = {
-        consensus_cluster(data = cdat, nk = k, reps = r,
-        algorithms = c("km", "pam"),
-        distance = c("eucl", "spear", "manh"),
-        prep.data = "none", seed.data = s,
-        file.name = outputFile)
-    },
-    rest = {
-        consensus_cluster(data = cdat, nk = k, reps = r,
-        algorithms = c("block"), #,"ap"), #,"gmm"),
-        prep.data = "none", seed.data = s,
-        file.name = outputFile)
-    }
+ssclust <- switch(
+  algs,
+  nmfbrunet = purrr::invoke(diceR::consensus_cluster, cc_args,
+                            algorithms = "nmf", nmf.method = "brunet"),
+  nmflee = purrr::invoke(diceR::consensus_cluster, cc_args,
+                         algorithms = "nmf", nmf.method = "lee"),
+  distalgs = purrr::invoke(diceR::consensus_cluster, cc_args,
+                           algorithms = c("km", "pam"),
+                           distance = c("eucl", "spear", "manh")),
+  rest = purrr::invoke(diceR::consensus_cluster, cc_args,
+                       algorithms = "block")
 )
