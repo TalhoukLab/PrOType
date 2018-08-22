d719499017baded26910c4ca52abe6f6403752b8
Derek Chiu <dchiu@bccrc.ca>
2018-08-22 16:32:32 -0700

    use outputDir to be consistent with Parameters

--- array_classifier/2_post_processing/7-validate_array.R
+++ array_classifier/2_post_processing/7-validate_array.R
@@ -4,7 +4,7 @@
 source(here::here("array_classifier/2_post_processing/utils/utils.R"))
 
 # Import cut 1 fits on top models
-all_fits <- list.files(file.path(output_dir, "fits"), full.names = TRUE) %>%
+all_fits <- list.files(file.path(outputDir, "fits"), full.names = TRUE) %>%
   purrr::set_names(stringr::str_remove(basename(.), paste0(trainSet, "_"))) %>%
   purrr::set_names(tools::file_path_sans_ext(names(.))) %>%
   purrr::map(readRDS)
@@ -19,7 +19,7 @@ data_overlap_array <-
 
 # Predict overlap array samples and combine with published
 cli::cat_line("Predicting overlap array samples")
-pred_dir <- file.path(output_dir, "predictions")
+pred_dir <- file.path(outputDir, "predictions")
 pred_overlap_array <- all_fits %>%
   purrr::map(splendid::prediction, data = data_overlap_array) %>%
   purrr::map(join_published_array, overlap = overlaps) %>%
@@ -30,7 +30,7 @@ saveRDS(pred_overlap_array, file.path(pred_dir, "pred_overlap_array.rds"))
 
 # Evaluate overlap array predictions using splendid and caret
 cli::cat_line("Evaluating overlap array predictions")
-eval_dir <- file.path(output_dir, "evals")
+eval_dir <- file.path(outputDir, "evals")
 eval_overlap_array <- pred_overlap_array %>%
   purrr::map(evaluate_array) %>%
   purrr::iwalk(~ saveRDS(.x, file.path(
--- nanostring_classifier/step3_ValidateNanoString.R
+++ nanostring_classifier/step3_ValidateNanoString.R
@@ -2,7 +2,7 @@
 source(here::here("nanostring_classifier/utils/utils.R"))
 
 # Import cut 1 fits on top models
-all_fits <- list.files(file.path(output_dir, "fits"), full.names = TRUE) %>%
+all_fits <- list.files(file.path(outputDir, "fits"), full.names = TRUE) %>%
   purrr::set_names(stringr::str_remove(basename(.), paste0(trainSet, "_"))) %>%
   purrr::set_names(tools::file_path_sans_ext(names(.))) %>%
   purrr::map(readRDS)
@@ -17,7 +17,7 @@ data_overlap_nstring <-
 
 # Predict overlap nstring samples and combine with published
 cli::cat_line("Predicting overlap nanostring samples")
-pred_dir <- file.path(output_dir, "predictions")
+pred_dir <- file.path(outputDir, "predictions")
 pred_overlap_nstring <- all_fits %>%
   purrr::map(splendid::prediction, data = data_overlap_nstring) %>%
   purrr::map(join_published_nstring, overlap = overlaps) %>%
@@ -27,14 +27,14 @@ pred_overlap_nstring <- all_fits %>%
 saveRDS(pred_overlap_nstring, file.path(pred_dir, "pred_overlap_nstring.rds"))
 
 # Import overlap array predictions
-pred_overlap_array <- readRDS(file.path(output_dir, "predictions", "pred_overlap_array.rds"))
+pred_overlap_array <- readRDS(file.path(outputDir, "predictions", "pred_overlap_array.rds"))
 
 # Combine overlapping array and nstring predictions
 pred_all <- purrr::map2(pred_overlap_array, pred_overlap_nstring, combine_pred)
 
 # Evaluate all overlap results using splendid and caret
 cli::cat_line("Evaluating overlap array and nanostring predictions")
-eval_dir <- file.path(output_dir, "evals")
+eval_dir <- file.path(outputDir, "evals")
 eval_overlap_all <- purrr::map(pred_all, evaluate_all) %>%
   purrr::iwalk(~ saveRDS(.x, file.path(
     eval_dir, paste0("eval_overlap_all_", .y, ".rds")
