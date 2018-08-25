aeb7d55ab8ad962f9d5e9434abd0eb2e910d8324
Derek Chiu <dchiu@bccrc.ca>
2018-08-24 17:10:20 -0700

    specify plotly settings to suppress warnings when saving widget

--- array_classifier/2_post_processing/1-evaluate_batch_effects.R
+++ array_classifier/2_post_processing/1-evaluate_batch_effects.R
@@ -67,12 +67,14 @@ for (dataSet in c(trainSet2, trainSet)) {
     cbind(pcaa$x[, 1:3])
 
   # PCA plots
-  p1 <- plotly::plot_ly(df, x = ~PC2, y = ~PC1, z = ~PC3, color = ~batch1)
+  p1 <- plotly::plot_ly(df, x = ~PC2, y = ~PC1, z = ~PC3, type = "scatter3d",
+                        mode = "markers", color = ~batch1)
   htmlwidgets::saveWidget(p1, file.path(outputDir, "plots",
                                         paste0(dataSet, "_pcaClust.html")),
                           selfcontained = FALSE)
 
-  p2 <- plotly::plot_ly(df, x = ~PC2, y = ~PC1, z = ~PC3, color = ~batch2)
+  p2 <- plotly::plot_ly(df, x = ~PC2, y = ~PC1, z = ~PC3, type = "scatter3d",
+                        mode = "markers", color = ~batch2)
   htmlwidgets::saveWidget(p2, file.path(outputDir, "plots",
                                         paste0(dataSet, "_pcastudy.html")),
                           selfcontained = FALSE)
