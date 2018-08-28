d079c0a605939e59e4aa2e30e895a3eb994ade49
Derek Chiu <dchiu@bccrc.ca>
2018-08-28 10:07:33 -0700

    relax file pattern, row bind with purrr

--- R/3_supervised/ivCombine.R
+++ R/3_supervised/ivCombine.R
@@ -1,12 +1,10 @@
-# command line arguments
-library(magrittr)
+`%>%` <- magrittr::`%>%`
 
 x <- list.files(file.path(outputDir, "supervised", "summary"),
                 recursive = TRUE,
                 full.names = TRUE,
-                pattern = "iv_summary_ov.*") %>%
-  purrr::map(~ readRDS(.)) %>%
-  data.table::rbindlist()
+                pattern = "iv_summary.*") %>%
+  purrr::map_df(readRDS)
 
 readr::write_rds(x, file.path(outputDir, "supervised", "summary", "iv_summary_COMBINED.rds"))
 x1 <- subset(x, x$measure == "accuracy")
