4ce19eb100ed458a6a78cfee358712f39c413b8f
Derek Chiu <dchiu@bccrc.ca>
2018-08-28 12:25:34 -0700

    add additional job to post-processing

--- R/5_post_processing/run_post_processing.sh
+++ R/5_post_processing/run_post_processing.sh
@@ -11,37 +11,38 @@ mkdir -p $outputDir/plots
 mkdir -p $outputDir/predictions
 
 Rname0=$workDir$dataSet/R_file/post_processing/baseline_validity.R
-Rname1=$workDir$dataSet/R_file/post_processing/evalute_batch.R
+Rname1=$workDir$dataSet/R_file/post_processing/evaluate_batch.R
 Rname2=$workDir$dataSet/R_file/post_processing/interval_validity.R
-Rname3=$workDir$dataSet/R_file/post_processing/mapping_predict.R
-
+Rname3=$workDir$dataSet/R_file/post_processing/validate_overlap.R
+Rname4=$workDir$dataSet/R_file/post_processing/mapping_predict.R
 
 shname0=$workDir$dataSet/sh_file/post_processing/baseline_validity.sh
-shname1=$workDir$dataSet/sh_file/post_processing/evalute_batch.sh
+shname1=$workDir$dataSet/sh_file/post_processing/evaluate_batch.sh
 shname2=$workDir$dataSet/sh_file/post_processing/interval_validity.sh
-shname3=$workDir$dataSet/sh_file/post_processing/mapping_predict.sh
+shname3=$workDir$dataSet/sh_file/post_processing/validate_overlap.sh
+shname4=$workDir$dataSet/sh_file/post_processing/mapping_predict.sh
 
 echo "Rscript $Rname0" > $shname0
 echo "Rscript $Rname1" > $shname1
 echo "Rscript $Rname2" > $shname2
 echo "Rscript $Rname3" > $shname3
+echo "Rscript $Rname4" > $shname4
 
 chmod +x $shname0
 chmod +x $shname1
 chmod +x $shname2
 chmod +x $shname3
+chmod +x $shname4
 
-
-echo 'trainSet <- "'$trainSet'"' > $Rname0
-echo 'trainSet2 <- "'$trainSet2'"' >> $Rname0
-echo 'testSet <- "'$testSet'"' >> $Rname0
+# echo 'trainSet <- "'$trainSet'"' > $Rname0
+# echo 'trainSet2 <- "'$trainSet2'"' >> $Rname0
+# echo 'testSet <- "'$testSet'"' >> $Rname0
 echo 'datasets <- c("'$trainSet'", "'$trainSet2'")' >> $Rname0
 echo 'outputDir <- "'$outputDir'"' >> $Rname0
 echo 'dataDir <- "assets/data"' >> $Rname0
 
 echo 'cli::cat_line("Validating Results")' >> $Rname0
 echo 'source("R/5_post_processing/0-validate_baseline_results.R")' >> $Rname0
-
 
 echo 'trainSet <- "'$trainSet'"' > $Rname1
 echo 'trainSet2 <- "'$trainSet2'"' >> $Rname1
@@ -64,20 +65,28 @@ echo 'cli::cat_line("Post-processing step 2")' >> $Rname2
 echo 'source("R/5_post_processing/2-internal_validity_plots.R")' >> $Rname2
 
 echo 'trainSet <- "'$trainSet'"' > $Rname3
-echo 'trainSet2 <- "'$trainSet2'"' >> $Rname3
-echo 'testSet <- "'$testSet'"' >> $Rname3
-echo 'datasets <- c("'$trainSet'", "'$trainSet2'")' >> $Rname3
 echo 'outputDir <- "'$outputDir'"' >> $Rname3
-echo 'dataDir <- "assets/data"' >> $Rname3
 
 echo 'cli::cat_line("Post-processing step 3")' >> $Rname3
-echo 'source("R/5_post_processing/predict_C2.R")' >> $Rname3
+echo 'source("R/5_post_processing/3-fit_top_models.R")' >> $Rname3
 echo 'cli::cat_line("Post-processing step 4")' >> $Rname3
-echo 'source("R/5_post_processing/probe_mapping_C2.R")' >> $Rname3
-echo 'cli::cat_line("Post-processing step 5")' >> $Rname3
-echo 'source("R/5_post_processing/mapping_signatures_C2.R")' >> $Rname3
+echo 'source("R/5_post_processing/4-validate_array.R")' >> $Rname3
 
-file_to_submit=($shname0 $shname1 $shname2 $shname3)
+echo 'trainSet <- "'$trainSet'"' > $Rname4
+echo 'trainSet2 <- "'$trainSet2'"' >> $Rname4
+echo 'testSet <- "'$testSet'"' >> $Rname4
+echo 'datasets <- c("'$trainSet'", "'$trainSet2'")' >> $Rname4
+echo 'outputDir <- "'$outputDir'"' >> $Rname4
+echo 'dataDir <- "assets/data"' >> $Rname4
+
+echo 'cli::cat_line("Post-processing step 5")' >> $Rname4
+echo 'source("R/5_post_processing/5-predict_C2.R")' >> $Rname4
+echo 'cli::cat_line("Post-processing step 6")' >> $Rname4
+echo 'source("R/5_post_processing/6-probe_mapping_C2.R")' >> $Rname4
+echo 'cli::cat_line("Post-processing step 7")' >> $Rname4
+echo 'source("R/5_post_processing/7-mapping_signatures_C2.R")' >> $Rname4
+
+file_to_submit=($shname0 $shname1 $shname2 $shname4)
 if command -v qsub &>/dev/null; then
   :
 else
