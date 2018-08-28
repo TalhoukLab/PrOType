f8b03eb6a6c65b14be7fd87f43c56b90f5ec8e0b
Derek Chiu <dchiu@bccrc.ca>
2018-08-28 14:51:25 -0700

    use cat msgs from master branch, missed qsub for shname3

--- R/5_post_processing/run_post_processing.sh
+++ R/5_post_processing/run_post_processing.sh
@@ -38,29 +38,29 @@ echo 'datasets <- c("'$trainSet'", "'$trainSet2'")' >> $Rname0
 echo 'outputDir <- "'$outputDir'"' >> $Rname0
 echo 'dataDir <- "assets/data"' >> $Rname0
 
-echo 'cli::cat_line("Validating Results")' >> $Rname0
+echo 'cli::cat_line("Validating baseline results")' >> $Rname0
 echo 'source("R/5_post_processing/0-validate_baseline_results.R")' >> $Rname0
 
 echo 'datasets <- c("'$trainSet'", "'$trainSet2'")' >> $Rname1
 echo 'outputDir <- "'$outputDir'"' >> $Rname1
 echo 'dataDir <- "assets/data"' >> $Rname1
 
-echo 'cli::cat_line("Post-processing step 1")' >> $Rname1
+echo 'cli::cat_line("Post-processing 1: evaluating batch effects")' >> $Rname1
 echo 'source("R/5_post_processing/1-evaluate_batch_effects.R")' >> $Rname1
 
 echo 'datasets <- c("'$trainSet'", "'$trainSet2'")' >> $Rname2
 echo 'outputDir <- "'$outputDir'"' >> $Rname2
 
-echo 'cli::cat_line("Post-processing step 2")' >> $Rname2
+echo 'cli::cat_line("Post-processing 2: internal validity plots")' >> $Rname2
 echo 'source("R/5_post_processing/2-internal_validity_plots.R")' >> $Rname2
 
 echo 'trainSet <- "'$trainSet'"' > $Rname3
 echo 'outputDir <- "'$outputDir'"' >> $Rname3
 echo 'dataDir <- "assets/data"' >> $Rname3
 
-echo 'cli::cat_line("Post-processing step 3")' >> $Rname3
+echo 'cli::cat_line("Post-processing 3: fitting top models")' >> $Rname3
 echo 'source("R/5_post_processing/3-fit_top_models.R")' >> $Rname3
-echo 'cli::cat_line("Post-processing step 4")' >> $Rname3
+echo 'cli::cat_line("Post-processing 4: validating overlap array")' >> $Rname3
 echo 'source("R/5_post_processing/4-validate_array.R")' >> $Rname3
 
 echo 'trainSet <- "'$trainSet'"' > $Rname4
@@ -68,14 +68,14 @@ echo 'testSet <- "'$testSet'"' >> $Rname4
 echo 'outputDir <- "'$outputDir'"' >> $Rname4
 echo 'dataDir <- "assets/data"' >> $Rname4
 
-echo 'cli::cat_line("Post-processing step 5")' >> $Rname4
+echo 'cli::cat_line("Post-processing 5: predict on cut2")' >> $Rname4
 echo 'source("R/5_post_processing/5-predict_C2.R")' >> $Rname4
-echo 'cli::cat_line("Post-processing step 6")' >> $Rname4
+echo 'cli::cat_line("Post-processing 6: probe mapping on cut2")' >> $Rname4
 echo 'source("R/5_post_processing/6-probe_mapping_C2.R")' >> $Rname4
-echo 'cli::cat_line("Post-processing step 7")' >> $Rname4
+echo 'cli::cat_line("Post-processing 7: mapping signatures on cut2")' >> $Rname4
 echo 'source("R/5_post_processing/7-mapping_signatures_C2.R")' >> $Rname4
 
-file_to_submit=($shname0 $shname1 $shname2 $shname4)
+file_to_submit=($shname0 $shname1 $shname2 $shname3 $shname4)
 if command -v qsub &>/dev/null; then
   :
 else
