c797dd3ac93d8eb2769222dac6587adb2ff70e62
Derek Chiu <dchiu@bccrc.ca>
2018-08-28 10:03:32 -0700

    top_models no longer part of supervised

--- R/3_supervised/ivSummary.sh
+++ R/3_supervised/ivSummary.sh
@@ -17,7 +17,7 @@ for dataset in "${dataSets[@]}"; do
 	Rname=$workDir/R_file/supervised/$dataset/iv.R
 	shname=$workDir/sh_file/supervised/$dataset/iv.sh
 
-    mkdir -p $outputDir'/supervised/summary/'$dataset
+  mkdir -p $outputDir'/supervised/summary/'$dataset
 
 	echo 'outputDir <- "'$outputDir'"' > $Rname
 	echo 'dataset <- "'$dataset'"' >> $Rname
@@ -36,29 +36,6 @@ for dataset in "${dataSets[@]}"; do
         bash $shname
     fi
 done
-
-
-mkdir -p $workDir/R_file/supervised
-mkdir -p $workDir/sh_file/supervised
-
-
-Rname=$workDir/R_file/supervised/top_models.R
-shname=$workDir/sh_file/supervised/top_models.sh
-
-echo 'outputDir <- "'$outputDir'"' > $Rname
-echo 'source("R/3_supervised/top_models.R")' >> $Rname
-
-echo "Rscript $Rname" > $shname
-chmod +x $shname
-
-# execute shell script to queue
-if command -v qsub &>/dev/null; then
-	echo "Adding To Queue: $shname"
-    file_to_submit+=($shname)
-else
-    echo "Making File"
-    bash $shname
-fi
 
 logDir=$baseLogDir'/supervised/ivSummary'
 if command -v qsub &>/dev/null; then
