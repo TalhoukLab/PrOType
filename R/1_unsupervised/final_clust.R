01182a8ebfcb4dfc4b3ac3992ad897d9df6667c9
Derek Chiu <dchiu@bccrc.ca>
2018-08-29 10:42:03 -0700

    set seed before rank aggregation since GA has randomness

--- R/1_unsupervised/final_clust.R
+++ R/1_unsupervised/final_clust.R
@@ -16,34 +16,34 @@ cl.mat <- purrr::map(cons.mat, hc, k = 4) %>%
   data.frame()
 
 final <- data.frame(
-  CSPA =     readRDS( file.path(ddir, paste0("cons_CSPA_", dataset, ".rds"))),
-  kmodes =   readRDS( file.path(ddir, paste0("cons_kmodes_", dataset, ".rds"))),
-  majority = readRDS( file.path(ddir, paste0("cons_majority_", dataset, ".rds"))),
-  cts =      readRDS( file.path(ddir, paste0("cons_LCEcts_", dataset, ".rds"))),
-  srs =      readRDS( file.path(ddir, paste0("cons_LCEsrs_", dataset, ".rds"))),
-  asrs =     readRDS( file.path(ddir, paste0("cons_LCEasrs_", dataset, ".rds"))),
+  CSPA =     readRDS(file.path(ddir, paste0("cons_CSPA_", dataset, ".rds"))),
+  kmodes =   readRDS(file.path(ddir, paste0("cons_kmodes_", dataset, ".rds"))),
+  majority = readRDS(file.path(ddir, paste0("cons_majority_", dataset, ".rds"))),
+  cts =      readRDS(file.path(ddir, paste0("cons_LCEcts_", dataset, ".rds"))),
+  srs =      readRDS(file.path(ddir, paste0("cons_LCEsrs_", dataset, ".rds"))),
+  asrs =     readRDS(file.path(ddir, paste0("cons_LCEasrs_", dataset, ".rds"))),
   cl.mat
 )
 
-cli::cat_line("Relabeling elements")
 # relabel the elements of the data frame
+cli::cat_line("Relabeling elements")
 finalR <- final
-finalR[] <- apply(final, 2, diceR::relabel_class, ref.cl = final[, referenceClass])
+finalR[] <-
+  apply(final, 2, diceR::relabel_class, ref.cl = final[, referenceClass])
 
+# evaluate clustering with internal validity indices
 cli::cat_line("Evaluating clustering")
-# Cluster evaluate at this point
 ii <- diceR:::ivi_table(finalR, cdat)
-ii <- list(ii)[[1]]
 
-cli::cat_line("Ranking aggregates")
-
-# Rank aggregate
+# rank aggregate
+cli::cat_line("Applying rank aggregation")
+set.seed(1)
 cr <- diceR:::consensus_rank(ii, n = 5)
 top <- cr$top.list
 ii <- ii[match(top, ii$Algorithms), ]
 finalR <- finalR[, top]
 
+# write to file
 cli::cat_line("Saving RDS")
 saveRDS(finalR, file.path(outputdir, "unsupervised", "final", dataset, paste0("all_clusts_", dataset, ".rds")))
 saveRDS(ii,     file.path(outputdir, "unsupervised", "final", dataset, paste0("ii_", dataset, ".rds")))
-print(ii)
