d8a8595992ba48c5db3ad93f3a0b9fc8550b5c2c
Derek Chiu <dchiu@bccrc.ca>
2018-08-29 10:15:30 -0700

    assign invocations to prevent Rplots.pdf from beinge generated

--- R/5_post_processing/1-internal_validity_plots.R
+++ R/5_post_processing/1-internal_validity_plots.R
@@ -7,11 +7,11 @@ source(here::here("R/5_post_processing/utils/utils.R"))
 plot_args <- list(dir = outputDir, datasets = datasets, print = FALSE)
 
 cli::cat_line("Plotting all ranked algorithms")
-purrr::invoke(all_algo_plot, plot_args, threshold = FALSE)
-purrr::invoke(all_algo_plot, plot_args, threshold = TRUE)
+p1 <- purrr::invoke(all_algo_plot, plot_args, threshold = FALSE)
+p2 <- purrr::invoke(all_algo_plot, plot_args, threshold = TRUE)
 
 cli::cat_line("Plotting top 2 algorithms with and without threshold")
-purrr::invoke(top2_algo_plot, plot_args)
+p3 <- purrr::invoke(top2_algo_plot, plot_args)
 
 cli::cat_line("Plotting algorithm by internal validity index heatmap")
-purrr::walk(datasets, algii_heatmap, dir = outputDir)
+p4 <- purrr::walk(datasets, algii_heatmap, dir = outputDir)
