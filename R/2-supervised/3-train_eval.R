6551c3e0ddf1f928c6cdd0d7eda0618c15fa763f
Derek Chiu <dchiu@bccrc.ca>
2018-09-13 13:10:13 -0700

    confirm eval merge can be done in one step

--- R/2-supervised/3-train_eval.R
+++ R/2-supervised/3-train_eval.R
@@ -1,22 +1,3 @@
-# `%>%` <- magrittr::`%>%`
-# # Evaluations merged by algorithm
-# files <- list.files(
-#   path = file.path(outputDir, "supervised", "reduce",
-#                    paste0(mname, "_", dataset)),
-#   pattern = paste0("_train_eval_", dataset),
-#   full.names = TRUE
-# )
-# # Sort first to fourth, then flatten list
-# eval_all <- files %>%
-#   `[`(c(1, 3, 4, 2)) %>%
-#   purrr::map(readRDS) %>%
-#   purrr::flatten()
-# # Write all evaluations merged
-# saveRDS(
-#   eval_all,
-#   file.path(outputDir, "supervised", "train_eval",
-#             paste0("train_eval_", dataset, ".rds"))
-# )
 `%>%` <- magrittr::`%>%`
 # All evaluation files
 eval_files <- algs %>%
@@ -37,5 +18,5 @@ eval_merged <- eval_files %>%
 saveRDS(
   eval_merged,
   file.path(outputDir, "supervised", "train_eval",
-            paste0("train_eval_", dataset, "2.rds"))
+            paste0("train_eval_", dataset, ".rds"))
 )
