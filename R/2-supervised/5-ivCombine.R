acd963565d8e85f0f43bc89c3c37cec453b97caf
Derek Chiu <dchiu@bccrc.ca>
2018-09-13 14:57:35 -0700

    fix sanity checking in ivCombine

--- R/2-supervised/5-ivCombine.R
+++ R/2-supervised/5-ivCombine.R
@@ -1,6 +1,6 @@
 `%>%` <- magrittr::`%>%`
 
-# Read in all iv_summary and row bind
+# Read in all iv_summary
 x <- list.files(
   path = file.path(outputDir, "supervised", "summary"),
   full.names = TRUE,
@@ -9,18 +9,23 @@ x <- list.files(
   grep(pattern = "(?<!threshold|COMBINED)\\.rds",
        perl = TRUE,
        value = TRUE) %>%
+  purrr::set_names(basename(dirname(.))) %>%
   purrr::map(readRDS) %>%
-  purrr::map_df(~ dplyr::mutate_at(., "batch_correction", as.character)) %>%
-  dplyr::mutate_at("batch_correction", as.factor)
+  purrr::map(dplyr::mutate_at, "batch_correction", as.character)
 
-# Sanity check the top accuracies per algorithm
+# Sanity check the top accuracies per algorithm for each dataset
 top_accuracy <- x %>%
-  dplyr::filter(measure == "accuracy") %>%
-  dplyr::arrange(dplyr::desc(percentile_50)) %>%
-  dplyr::distinct()
+  purrr::map(~ {
+    dplyr::filter(., measure == "accuracy") %>%
+      dplyr::arrange(dplyr::desc(percentile_50)) %>%
+      dplyr::distinct()
+  })
 
 # Write out iv_summary combined
+ivs_combined <- x %>%
+  dplyr::bind_rows() %>%
+  dplyr::mutate_at("batch_correction", as.factor)
 saveRDS(
-  object = x,
+  object = ivs_combined,
   file = file.path(outputDir, "supervised", "summary", "iv_summary_COMBINED.rds")
 )
