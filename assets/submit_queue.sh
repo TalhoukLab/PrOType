94eae87c9c76d038eb1a9ea8ec18a6ac3e2dd5b1
Derek Chiu <dchiu@bccrc.ca>
2018-09-25 16:35:03 -0700

    only show queue status for jobs submitted from one command

--- assets/submit_queue.sh
+++ assets/submit_queue.sh
@@ -13,12 +13,12 @@ function err_msg() {
 
 # Queue status for user without the column headers
 function qstatu() {
-  qstat -u $1 | grep -r ".*$1.*"
+  qstat -u $1 | grep -r ".*$1.*" | grep -E $2
 }
 
 # Current number of jobs in queue for user
 function njobs() {
-  qstatu $1 | wc -l
+  qstatu $1 $2 | wc -l
 }
 
 # Elapsed time since first job submitted for user.
@@ -26,9 +26,9 @@ function njobs() {
 # Reference: https://www.linuxquestions.org/questions/linux-newbie-8/time-difference-calculation-4175459414/
 function elapsed() {
   if [[ `njobs $1` > 0 ]]; then
-    first=`qstatu $1 | awk '{print $7}' | head -n 1`
+    first=`qstatu $1 $2 | awk '{print $7}' | head -n 1`
   else
-    first=$2
+    first=$3
   fi
   now=$(date +"%T")
   SEC1=`date +%s -d ${first}`
@@ -40,6 +40,9 @@ function elapsed() {
 # Print number of total jobs to submit
 jtotal=${#file_to_submit[@]}
 echo -e "$BLUE_BULLET Total jobs: $jtotal"
+
+# Store job ids in a job array
+jobarray=()
 
 # Store start time
 start=$(date +"%T")
@@ -69,6 +72,8 @@ for shname in "${file_to_submit[@]}"; do
       # Submit shell script to queue without extra verbosity using variable qq
 		  qcmd="qsub -V -p -1 -l mem_free=$mem_free -l mem_token=$mem_token -l h_vmem=$h_vmem -e $logDir -o $logDir -q all.q $shname"
       qq=`$qcmd`
+      jobid=$(echo $qcmd | awk '{print $3}')
+      jobarray+=($jobid)
       echo -e "$GREEN_TICK Submitted to queue: $shname"
   fi
 done
@@ -76,21 +81,24 @@ echo -e "$GREEN_TICK Job submission finished. Check status with \"qstat -u ${use
 
 # Step 2: wait until all jobs are complete before proceeding
 
+# All job ids concatenated for regex pattern
+pat=$(echo ${jobarray[@]} | tr " " "|")
+
 # Initialize queue length at starting number of jobs
 if command -v qsub &>/dev/null; then
-  currQLength=`njobs $user`
+  currQLength=`njobs $user $pat`
 else
   currQLength=0
 fi
 
 while [[ $currQLength > 0 && shouldWait ]]; do
-    currQLength=`njobs $user`
+    currQLength=`njobs $user $pat`
 
     if [[ $currQLength > 0 ]]; then
-      echo -e "$BLUE_BULLET Time elapsed: `elapsed ${user} ${start}` | Jobs remaining: ${currQLength}/${jtotal}"
+      echo -e "$BLUE_BULLET Time elapsed: `elapsed ${user} ${pat} ${start}` | Jobs remaining: ${currQLength}/${jtotal}"
       sleep 30s
     else
-      echo -e "$GREEN_TICK All jobs completed in: `elapsed ${user} ${start}`"
+      echo -e "$GREEN_TICK All jobs completed in: `elapsed ${user} ${pat} ${start}`"
     fi
 done
 
