f108181a1f9062370bb7f0ffa7178dcd106516d2
Derek Chiu <dchiu@bccrc.ca>
2018-10-31 15:42:52 -0700

    merge all gs utility functions into a single utils.R

--- pipeline/5-gene_selection/0-gs_setup.R
+++ pipeline/5-gene_selection/0-gs_setup.R
@@ -1,18 +1,7 @@
 # Gene Selection setup ----------------------------------------------------
 
 # Load utility functions
-GS_training_dir <- "pipeline/5-gene_selection/scripts"
-GS_training_files <- c(
-  "define.R",
-  "train.R",
-  "bootstrap.R",
-  "evaluate.R",
-  "summary.R",
-  "analysis.R",
-  "final.R",
-  "utils.R"
-)
-purrr::walk(here::here(GS_training_dir, GS_training_files), source)
+source(here::here("pipeline/5-gene_selection/utils.R"))
 
 # Load NanoString data and prediction labels
 nsdat <- load_nanostring()
