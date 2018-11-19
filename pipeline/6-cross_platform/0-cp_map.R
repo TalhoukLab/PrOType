3e243f372648c57375ba03c535cb919b404d9509
Derek Chiu <dchiu@bccrc.ca>
2018-11-19 10:12:31 -0800

    old utils file location

--- pipeline/6-cross_platform/0-cp_map.R
+++ pipeline/6-cross_platform/0-cp_map.R
@@ -1,5 +1,5 @@
 # Load utility functions
-source(here::here("pipeline/5-gene_selection/scripts/utils.R"))
+source(here::here("pipeline/5-gene_selection/utils.R"))
 nstring_path <- "data/nstring"
 array_path <- "data/array/OverlapSet"
 
