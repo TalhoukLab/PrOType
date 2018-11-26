0d3dd2889a9422962f04c456e5d9ad3dfa763937
Derek Chiu <dchiu@bccrc.ca>
2018-11-26 14:45:15 -0800

    split nanostring pipeline into three jobs

---
+++ pipeline/4-nanostring_classifier/3-tcga_model.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+. ./Parameters.sh
+
+# Make directories for R script, shell script
+subDir=nanostring/predictions
+RSubDir=$RDir/$subDir
+shSubDir=$shDir/$subDir
+mkdir -p $RSubDir
+mkdir -p $shSubDir
+mkdir -p $outputDir/$subDir
+
+# Content of R file
+R_file=$RSubDir/tcga_model.R
+echo 'outputDir <- "'$outputDir'"' > $R_file
+echo 'trainSet <- "'$trainSet'"' >> $R_file
+echo 'cli::cat_line("NanoString classifier 3: predicting full NanoString data using TCGA model")' >> $R_file
+echo 'source("pipeline/4-nanostring_classifier/3-tcga_model.R")' >> $R_file
+
+# Content of sh file
+sh_file=$shSubDir/tcga_model.sh
+echo "Rscript $R_file" > $sh_file
+chmod +x $sh_file
+Rscript $R_file
+
+# Add to queue if qsub exists
+if command -v qsub &>/dev/null; then
+  file_to_submit+=($sh_file)
+  echo -e "$GREEN_TICK Added to queue: $sh_file"
+fi
+
+# Submit to queue if qsub exists
+logDir=$logDir/$subDir
+outputDir=$outputDir/$subDir
+if command -v qsub &>/dev/null; then
+  . ./assets/submit_queue.sh
+fi
