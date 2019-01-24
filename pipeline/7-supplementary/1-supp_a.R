2fcfe4695894078f581a5ebe5f5e700d77dd0f56
Derek Chiu <dchiu@bccrc.ca>
2019-01-24 12:30:45 -0800

    refactor render into purl, pass outputDir from bash

--- pipeline/7-supplementary/1-supp_a.R
+++ pipeline/7-supplementary/1-supp_a.R
@@ -1,5 +1,9 @@
-rmarkdown::render(
-  input = "pipeline/7-supplementary/Supp_A.Rmd",
-  output_dir = file.path(outputDir, "supplementary/reports"),
-  params = list(outputDir = outputDir)
-)
+# rmarkdown::render(
+#   input = "pipeline/7-supplementary/Supp_A.Rmd",
+#   output_dir = file.path(outputDir, "supplementary/reports"),
+#   params = list(outputDir = outputDir)
+# )
+file_name <- "pipeline/7-supplementary/Supp_A"
+knitr::purl(input = paste0(file_name, ".Rmd"),
+            output = paste0(file_name, ".R"))
+source(paste0(file_name, ".R"))
---
+++ pipeline/7-supplementary/Supp_A.R
@@ -0,0 +1,117 @@
+## ----setup_A, include=FALSE----------------------------------------------
+knitr::opts_chunk$set(
+	echo = FALSE,
+	message = FALSE,
+	warning = FALSE
+)
+params <- list(outputDir = outputDir)
+
+## ----child="Supp_A05.Rmd"------------------------------------------------
+
+## ----setup_A05, include=FALSE--------------------------------------------
+knitr::opts_chunk$set(
+	echo = FALSE,
+	message = FALSE,
+	warning = FALSE,
+	results = "asis"
+)
+
+## ----load_A05------------------------------------------------------------
+# Load packages, helpers, input data
+library(pander)
+library(here)
+source(here("pipeline/7-supplementary/utils.R"))
+
+panderOptions("keep.trailing.zeros", FALSE)
+panderOptions("table.split.table", Inf)
+
+aa_raw <- readRDS(file.path(
+  params$outputDir,
+  "unsupervised/map_genes/ov.afc1_xpn/npcp-hcNorm_ov.afc1_xpn.rds"
+))
+aa_clusts <- readRDS(file.path(
+  params$outputDir,
+  "unsupervised/final/ov.afc1_xpn/all_clusts_ov.afc1_xpn.rds"
+))
+tcga_dat <- readr::read_csv(
+  here("data/nstring/OTTA_Cohorts_20171103_AFFY_MA.csv"),
+  col_types = readr::cols()
+)
+load(here("data/nstring/tcga_data_with_molsubtypes.rda")) # eset
+
+## ----aocs_table----------------------------------------------------------
+# AOCS samples in OTTA cohort
+aocs_tab <- matrix(
+  c(76, 3, 0, 1, 4, 42, 0, 1, 0, 5, 46, 1, 3, 0, 0, 33),
+  nrow = 4, 
+  byrow = TRUE,
+  dimnames = list(
+    `AOCS Predicted Labels` = c("C1.MES", "C2.IMM", "C4.DIF", "C5.PRO"),
+    `AOCS Published Labels` = c("C1.MES", "C2.IMM", "C4.DIF", "C5.PRO")
+  )
+) %>% 
+  as.table()
+
+## ----aocs_confmat--------------------------------------------------------
+aocs_confmat <- caret::confusionMatrix(aocs_tab)
+pandoc.table(ftable(aocs_confmat[["table"]]))
+
+## ----aocs_ov_metrics-----------------------------------------------------
+aocs_confmat_ov_metrics <- confmat_metrics(aocs_confmat, metrics = "overall")
+pandoc.table(aocs_confmat_ov_metrics)
+
+## ----aocs_bc_metrics-----------------------------------------------------
+aocs_confmat_bc_metrics <- confmat_metrics(aocs_confmat, metrics = "byclass")
+pandoc.table(aocs_confmat_bc_metrics, keep.trailing.zeros = TRUE)
+
+## ----aa_kmodes-----------------------------------------------------------
+# All-array samples
+aa_samples <- gsub("_", "-", stringr::str_sub(rownames(aa_raw), 15, 26))
+
+# All-array kmodes clustering labels
+aa_kmodes <- aa_clusts %>% 
+  dplyr::transmute(
+    sample = aa_samples,
+    kmodes = factor(kmodes,
+                    labels = c("C2.IMM", "C4.DIF", "C5.PRO", "C1.MES")) %>% 
+      relevel("C1.MES")
+  ) %>%
+  dplyr::filter(grepl("TCGA", sample)) %>% 
+  tibble::as_tibble()
+
+## ----tcga_training-------------------------------------------------------
+# TCGA samples
+tcga_samples <- tcga_dat %>%
+  dplyr::filter(StudyID == "TCGA", `Included Post 20171103 = 1` == 1) %>%
+  dplyr::mutate(SAMPLEID = gsub("_", "-", substr(Label, 15, 26))) %>%
+  dplyr::pull()
+
+# TCGA training set labels from expression set for included samples
+tcga_training <- eset %>%
+  `[`(, intersect(tcga_samples, Biobase::sampleNames(.))) %>% {
+    tibble::tibble(
+      sample = Biobase::sampleNames(.),
+      tcga_labels = factor(.[["MolSubtype"]],
+                           labels = c("C1.MES", "C2.IMM", "C4.DIF", "C5.PRO"))
+    )
+  }
+
+## ----aa_vs_tcga_confmat--------------------------------------------------
+# Compare all-array kmodes clustering with TCGA training set labels
+aa_vs_tcga <- dplyr::inner_join(aa_kmodes, tcga_training, by = "sample") %>% 
+  with(., table(
+    `All-Array K-modes Clustering Labels` = kmodes,
+    `TCGA Training Labels` = tcga_labels
+  )) %>% 
+  caret::confusionMatrix()
+pandoc.table(ftable(aa_vs_tcga[["table"]]))
+
+## ----aa_vs_tcga_ov_metrics-----------------------------------------------
+aa_vs_tcga_ov_metrics <- confmat_metrics(aa_vs_tcga, metrics = "overall")
+pandoc.table(aa_vs_tcga_ov_metrics)
+
+## ----aa_vs_tcga_bc_metrics-----------------------------------------------
+aa_vs_tcga_bc_metrics <- confmat_metrics(aa_vs_tcga, metrics = "byclass")
+pandoc.table(aa_vs_tcga_bc_metrics, keep.trailing.zeros = TRUE)
+
+
