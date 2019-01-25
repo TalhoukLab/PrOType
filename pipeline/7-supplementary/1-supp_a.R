1e3c6dce56cd9557b8bafd522536547839db9554
Derek Chiu <dchiu@bccrc.ca>
2019-01-24 16:16:00 -0800

    turn off all warnings as they get written to stderr

--- pipeline/7-supplementary/1-supp_a.R
+++ pipeline/7-supplementary/1-supp_a.R
@@ -6,4 +6,6 @@
 file_name <- "pipeline/7-supplementary/Supp_A"
 knitr::purl(input = paste0(file_name, ".Rmd"),
             output = paste0(file_name, ".R"))
+options(warn = -1)
 source(paste0(file_name, ".R"))
+options(warn = 0)
